216c4afdd464dabb1824e9c520216b86236dd882
luodan.wg <luodan.wg@gmail.com>
2017-01-13 00:20:23 +0800

    update common/serialization to be compatible of onchain c# date

--- common/serialization/serialize.go
+++ common/serialization/serialize.go
@@ -1,18 +1,17 @@
 package serialization
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"io"
 	"math"
-	"sync"
 )
 
-var bufPool = sync.Pool{New: func() interface{} { return new([10]byte) }}
-var readerPool = sync.Pool{New: func() interface{} { return new(byteReader) }}
-var ErrRange = errors.New("value out of range")
-
 //SerializableData describe the data need be serialized.
+var ErrRange = errors.New("value out of range")
+var ErrEof = errors.New("got EOF, can not get the next byte")
+
 type SerializableData interface {
 
 	//Write data to writer
@@ -24,7 +23,7 @@ type SerializableData interface {
 
 func WriteDataList(w io.Writer, list []SerializableData) error {
 	len := uint64(len(list))
-	WriteVarUint64(w, len)
+	WriteVarUint(w, len)
 
 	for _, data := range list {
 		data.Serialize(w)
@@ -33,171 +32,240 @@ func WriteDataList(w io.Writer, list []SerializableData) error {
 	return nil
 }
 
-func ReadVarInt32(r io.Reader) (int32, int, error) {
-	br := getReader(r)
-	defer readerPool.Put(br)
-	val, err := binary.ReadVarint(br)
+/*
+ ******************************************************************************
+ * public func for outside calling
+ ******************************************************************************
+ * 1. WriteVarUint func, depend on the inpute number's Actual number size,
+ *    serialize to bytes.
+ *      uint8  =>  (LittleEndian)num in 1 byte                 = 1bytes
+ *      uint16 =>  0xfd(1 byte) + (LittleEndian)num in 2 bytes = 3bytes
+ *      uint32 =>  0xfe(1 byte) + (LittleEndian)num in 4 bytes = 5bytes      
+ *      uint64 =>  0xff(1 byte) + (LittleEndian)num in 8 bytes = 9bytes    
+ * 2. ReadVarUint  func, this func will read the first byte to determined 
+ *    the num length to read.and retrun the uint64
+ *      first byte = 0xfd, read the next 2 bytes as uint16
+ *      first byte = 0xfe, read the next 4 bytes as uint32
+ *      first byte = 0xff, read the next 8 bytes as uint64
+ *      other else,        this byte as uint8
+ * 3. WriteVarBytes func, this func will output two item as serialization.
+ *      length of bytes (uint8/uint16/uint32/uint64)  +  bytes
+ * 4. WriteVarString func, this func will output two item as serialization.
+ *      length of string(uint8/uint16/uint32/uint64)  +  bytes(string)
+ * 5. ReadVarBytes func, this func will first read a uint to identity the 
+ *    length of bytes, and use it to get the next bytes as a bytes.
+ * 6. ReadVarString func, this func will first read a uint to identity the 
+ *    length of string, and use it to get the next bytes as a bytes.
+ ******************************************************************************
+ */
+func WriteVarUint(writer io.Writer, value uint64) error {
+	b_buf := new(bytes.Buffer)
+	if value < 0xFD {
+		valx := uint8(value)
+		err := binary.Write(b_buf, binary.LittleEndian, valx)
+		if err != nil {
+			return err
+		}
+	} else if value <= 0xFFFF {
+		err := b_buf.WriteByte(0xFD)
+		if err != nil {
+			return err
+		}
+		valx := uint16(value)
+		err = binary.Write(b_buf, binary.LittleEndian, valx)
+		if err != nil {
+			return err
+		}
+	} else if value <= 0xFFFFFFFF {
+		err := b_buf.WriteByte(0xFE)
+		if err != nil {
+			return err
+		}
+		valx := uint32(value)
+		err = binary.Write(b_buf, binary.LittleEndian, valx)
+		if err != nil {
+			return err
+		}
+	} else {
+		err := b_buf.WriteByte(0xFF)
+		if err != nil {
+			return err
+		}
+		valx := uint64(value)
+		err = binary.Write(b_buf, binary.LittleEndian, valx)
+		if err != nil {
+			return err
+		}
+	}
+	_, err := writer.Write(b_buf.Bytes())
 	if err != nil {
-		return 0, br.n, err
+		return err
 	}
-	if val > math.MaxInt32 || val < math.MinInt32 {
-		return 0, br.n, ErrRange
-	}
-	return int32(val), br.n, nil
+	return nil
 }
 
-func ReadVarInt64(r io.Reader) (int64, int, error) {
-	br := getReader(r)
-	defer readerPool.Put(br)
-	val, err := binary.ReadVarint(br)
+func ReadVarUint(r io.Reader, maxint uint64) (uint64, error) {
+	if maxint == 0x00 {
+		maxint = math.MaxUint64
+	}
+	fb, _ := byteReader(r)
+	if bytes.Equal(fb, []byte{byte(0xfd)}) {
+		value, err := readerUint16(r)
+		if err != nil {
+			return 0, err
+		}
+		if value > maxint {
+			return 0, ErrRange
+		}
+		return value, nil
+	} else if bytes.Equal(fb, []byte{byte(0xfe)}) {
+		value, err := readerUint32(r)
+		if err != nil {
+			return 0, err
+		}
+		if value > maxint {
+			return 0, ErrRange
+		}
+		return value, nil
+	} else if bytes.Equal(fb, []byte{byte(0xff)}) {
+		value, err := readerUint64(r)
+		if err != nil {
+			return 0, err
+		}
+		if value > maxint {
+			return 0, ErrRange
+		}
+		return value, nil
+	} else {
+		value, err := readerUint8(fb)
+		if err != nil {
+			return 0, err
+		}
+		if value > maxint {
+			return 0, ErrRange
+		}
+		return value, nil
+	}
+
+	return 0, nil
+}
+
+func WriteVarBytes(writer io.Writer, value []byte) error {
+	err := WriteVarUint(writer, uint64(len(value)))
 	if err != nil {
-		return 0, br.n, err
+		return err
 	}
-	if val > math.MaxInt64 || val < math.MinInt64 {
-		return 0, br.n, ErrRange
-	}
-	return int64(val), br.n, nil
-}
-
-func ReadVarUint32(r io.Reader) (uint32, int, error) {
-	br := getReader(r)
-	defer readerPool.Put(br)
-	val, err := binary.ReadUvarint(br)
+	_, err = writer.Write(value)
 	if err != nil {
-		return 0, br.n, err
+		return err
 	}
-	if val > math.MaxUint32 {
-		return 0, br.n, ErrRange
-	}
-	return uint32(val), br.n, nil
+	return nil
 }
 
-func ReadVarUint64(r io.Reader) (uint64, int, error) {
-	br := getReader(r)
-	defer readerPool.Put(br)
-	val, err := binary.ReadUvarint(br)
+func WriteVarString(writer io.Writer, value string) error {
+	err := WriteVarUint(writer, uint64(len(value)))
 	if err != nil {
-		return 0, br.n, err
+		return err
 	}
-	if val > math.MaxUint64 {
-		return 0, br.n, ErrRange
-	}
-	return val, br.n, nil
-}
-
-func ReadVarBytes(r io.Reader) ([]byte, int, error) {
-	len, n, err := ReadVarUint32(r)
+	_, err = writer.Write([]byte(value))
 	if err != nil {
-		return nil, n, err
+		return err
 	}
-	if len == 0 {
-		return nil, n, nil
-	}
-	buf := make([]byte, len)
-	n2, err := io.ReadFull(r, buf)
-	return buf, n + n2, err
+	return nil
 }
 
-func ReadVarstring(r io.Reader) (string, int, error) {
-
-	x, n, err := ReadVarBytes(r)
+func ReadVarBytes(reader io.Reader) ([]byte, error) {
+	val, err := ReadVarUint(reader, 0)
 	if err != nil {
-		return "", n, err
+		return nil, err
 	}
-	str := string(x)
-	return str, n, err
-}
-
-func WriteVarUint32(w io.Writer, val uint32) (int, error) {
-	if val > math.MaxUint32 {
-		return 0, ErrRange
-	}
-	valx := uint64(val)
-	buf := bufPool.Get().(*[10]byte)
-	n := binary.PutUvarint(buf[:], valx)
-	b, err := w.Write(buf[:n])
-	bufPool.Put(buf)
-	return b, err
-}
-
-func WriteVarInt32(w io.Writer, val int32) (int, error) {
-	if val > math.MaxInt32 || val < math.MinInt32 {
-		return 0, ErrRange
-	}
-	valx := int64(val)
-	buf := bufPool.Get().(*[10]byte)
-	n := binary.PutVarint(buf[:], valx)
-	b, err := w.Write(buf[:n])
-	bufPool.Put(buf)
-	return b, err
-}
-
-func WriteVarInt64(w io.Writer, val int64) (int, error) {
-	if val > math.MaxInt64 || val < math.MinInt64 {
-		return 0, ErrRange
-	}
-	buf := bufPool.Get().(*[10]byte)
-	n := binary.PutVarint(buf[:], val)
-	b, err := w.Write(buf[:n])
-	bufPool.Put(buf)
-	return b, err
-}
-
-func WriteVarUint64(w io.Writer, val uint64) (int, error) {
-	if val > math.MaxUint64 {
-		return 0, ErrRange
-	}
-	buf := bufPool.Get().(*[10]byte)
-	n := binary.PutUvarint(buf[:], val)
-	b, err := w.Write(buf[:n])
-	bufPool.Put(buf)
-	return b, err
-}
-
-func WriteVarBytes(w io.Writer, str []byte) (int, error) {
-	n, err := WriteVarUint32(w, uint32(len(str)))
+	str, err := byteXReader(reader, val)
 	if err != nil {
-		return n, err
+		return nil, err
 	}
-	n2, err := w.Write(str)
-	return n + n2, err
+	return str, nil
 }
 
-func WriteVarString(w io.Writer, str string) (int, error) {
-	buf := []byte(str)
-	n, err := WriteVarBytes(w, buf)
+func ReadVarString(reader io.Reader) (string, error) {
+	val, err := ReadVarBytes(reader)
 	if err != nil {
-		return n, err
+		return "", err
 	}
-	return n, err
+	return string(val), nil
 }
 
-func getReader(r io.Reader) *byteReader {
-	br := readerPool.Get().(*byteReader)
-	br.reset(r)
-	return br
-}
-
-type byteReader struct {
-	n int
-	r io.Reader
-	e error
-	b [1]byte
-}
-
-func (r *byteReader) reset(reader io.Reader) {
-	*r = byteReader{n: 0, r: reader, e: nil}
-}
-
-func (r *byteReader) ReadByte() (byte, error) {
-	if r.e != nil {
-		return 0, r.e
-	}
-	n, err := r.r.Read(r.b[:])
+//**************************************************************************
+//**    internal func                                                    ***
+//**************************************************************************
+func byteReader(reader io.Reader) ([]byte, error) {
+	p := make([]byte, 1)
+	n, err := reader.Read(p)
 	if n > 0 {
-		r.e = err
-		r.n++
-		return r.b[0], nil
+		return p[:], nil
 	}
-	return 0, err
+	return p, err
+}
+
+func byteXReader(reader io.Reader, x uint64) ([]byte, error) {
+	p := make([]byte, x)
+	n, err := reader.Read(p)
+	if n > 0 {
+		return p[:], nil
+	}
+	return p, err
+}
+
+func readerUint8(bytex []byte) (uint64, error) {
+	b_buf := bytes.NewBuffer(bytex)
+	var x uint8
+	err := binary.Read(b_buf, binary.LittleEndian, &x)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(x), nil
+}
+
+func readerUint16(reader io.Reader) (uint64, error) {
+	p := make([]byte, 2)
+	n, err := reader.Read(p)
+	if n <= 0 || err != nil {
+		return 0, ErrEof
+	}
+	b_buf := bytes.NewBuffer(p)
+	var x uint16
+	err = binary.Read(b_buf, binary.LittleEndian, &x)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(x), nil
+}
+
+func readerUint32(reader io.Reader) (uint64, error) {
+	p := make([]byte, 4)
+	n, err := reader.Read(p)
+	if n <= 0 || err != nil {
+		return 0, ErrEof
+	}
+	b_buf := bytes.NewBuffer(p)
+	var x uint32
+	err = binary.Read(b_buf, binary.LittleEndian, &x)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(x), nil
+}
+
+func readerUint64(reader io.Reader) (uint64, error) {
+	p := make([]byte, 8)
+	n, err := reader.Read(p)
+	if n <= 0 || err != nil {
+		return 0, ErrEof
+	}
+	b_buf := bytes.NewBuffer(p)
+	var x uint64
+	err = binary.Read(b_buf, binary.LittleEndian, &x)
+	if err != nil {
+		return 0, err
+	}
+	return x, nil
 }
--- common/serialization/serialize_test.go
+++ common/serialization/serialize_test.go
@@ -2,71 +2,16 @@ package serialization
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"math"
-	"strings"
 	"testing"
 )
 
-func BenchmarkReadVarUint32(b *testing.B) {
-	data := []byte{0xff, 0xff, 0xff, 0xff, 0x01}
-	r := bytes.NewReader(data)
-	for i := 0; i < b.N; i++ {
-		ReadVarUint32(r)
-	}
-}
-
-func BenchmarkReadVarInt32(b *testing.B) {
-	data := []byte{0xff, 0xff, 0xff, 0xff, 0x01}
-	r := bytes.NewReader(data)
-	for i := 0; i < b.N; i++ {
-		ReadVarUint32(r)
-	}
-}
-
-func BenchmarkReadVarUint64(b *testing.B) {
-	data := []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}
-	r := bytes.NewReader(data)
-	for i := 0; i < b.N; i++ {
-		//r.Reset(data)
-		ReadVarUint64(r)
-	}
-}
-
-func BenchmarkReadVarInt64(b *testing.B) {
-	data := []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}
-	r := bytes.NewReader(data)
-	for i := 0; i < b.N; i++ {
-		//r.Reset(data)
-		ReadVarUint64(r)
-	}
-}
-
-func BenchmarkWriteVarUint32(b *testing.B) {
-	n := uint32(math.MaxUint32)
-	for i := 0; i < b.N; i++ {
-		WriteVarUint32(ioutil.Discard, n)
-	}
-}
-
-func BenchmarkWriteVarInt32(b *testing.B) {
-	n := int32(math.MaxInt32)
-	for i := 0; i < b.N; i++ {
-		WriteVarInt32(ioutil.Discard, n)
-	}
-}
-
-func BenchmarkWriteVarUint64(b *testing.B) {
+func BenchmarkWriteVarUint(b *testing.B) {
 	n := uint64(math.MaxUint64)
 	for i := 0; i < b.N; i++ {
-		WriteVarUint64(ioutil.Discard, n)
-	}
-}
-
-func BenchmarkWriteVarInt64(b *testing.B) {
-	n := int64(math.MaxInt64)
-	for i := 0; i < b.N; i++ {
-		WriteVarInt64(ioutil.Discard, n)
+		WriteVarUint(ioutil.Discard, n)
 	}
 }
 
@@ -86,270 +31,48 @@ func BenchmarkWriteVarString(b *testing.B) {
 	}
 }
 
-func TestVarUint32(t *testing.T) {
-	cases := []struct {
-		n       uint32
-		want    []byte
-		wantErr error
-	}{
-		{
-			n:    0,
-			want: []byte{0},
-		},
-		{
-			n:    500,
-			want: []byte{0xf4, 0x03},
-		},
-		{
-			n:    math.MaxUint32,
-			want: []byte{0xff, 0xff, 0xff, 0xff, 0x0f},
-		},
-	}
-
-	for _, c := range cases {
-		b := new(bytes.Buffer)
-		n, err := WriteVarUint32(b, c.n)
-		if c.wantErr != err {
-			t.Errorf("WriteVarUint32(%d): err %v, want %v", c.n, err, c.wantErr)
-			continue
-		}
-		if c.wantErr != nil {
-			continue
-		}
-		if n != len(c.want) {
-			t.Errorf("WriteVarUint32(%d): wrote %d byte(s), want %d", c.n, n, len(c.want))
-		}
-		if !bytes.Equal(c.want, b.Bytes()) {
-			t.Errorf("WriteVarUint32(%d): got %x, want %x", c.n, b.Bytes(), c.want)
-		}
-		b = bytes.NewBuffer(b.Bytes())
-		v, n, err := ReadVarUint32(b)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if n != len(c.want) {
-			t.Errorf("ReadVarUint32 [c.n = %d] got %d bytes, want %d", c.n, n, len(c.want))
-		}
-		if uint32(v) != c.n {
-			t.Errorf("ReadVarUint32 got %d, want %d", v, c.n)
-		}
-	}
+func BenchmarkReadVarUint(b *testing.B) {
+	data := []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
+	r := bytes.NewReader(data)
+	ReadVarUint(r, 0)
 }
 
-func TestVarInt32(t *testing.T) {
-	cases := []struct {
-		n       int32
-		want    []byte
-		wantErr error
-	}{
-		{
-			n:    0,
-			want: []byte{0},
-		},
-		{
-			n:    500,
-			want: []byte{0xe8, 0x07},
-		},
-		{
-			n:    math.MaxInt32,
-			want: []byte{0xfe, 0xff, 0xff, 0xff, 0x0f},
-		},
-	}
-
-	for _, c := range cases {
-		b := new(bytes.Buffer)
-		n, err := WriteVarInt32(b, c.n)
-		if c.wantErr != err {
-			t.Errorf("WriteVarInt32(%d): err %v, want %v", c.n, err, c.wantErr)
-			continue
-		}
-		if c.wantErr != nil {
-			continue
-		}
-		if n != len(c.want) {
-			t.Errorf("WriteVarIint32(%d): wrote %d byte(s), want %d", c.n, n, len(c.want))
-		}
-		if !bytes.Equal(c.want, b.Bytes()) {
-			t.Errorf("WriteVarIint32(%d): got %x, want %x", c.n, b.Bytes(), c.want)
-		}
-		b = bytes.NewBuffer(b.Bytes())
-		v, n, err := ReadVarInt32(b)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if n != len(c.want) {
-			t.Errorf("ReadVarInt32 [c.n = %d] got %d bytes, want %d", c.n, n, len(c.want))
-		}
-		if int32(v) != c.n {
-			t.Errorf("ReadVarInt32 got %d, want %d", v, c.n)
-		}
-	}
+func BenchmarkReadVarBytes(b *testing.B) {
+	data := []byte{10, 11, 12}
+	r := bytes.NewReader(data)
+	ReadVarBytes(r)
 }
 
-func TestVarUint64(t *testing.T) {
-	cases := []struct {
-		n       uint64
-		want    []byte
-		wantErr error
-	}{
-		{
-			n:    0,
-			want: []byte{0},
-		},
-		{
-			n:    500,
-			want: []byte{0xf4, 0x03},
-		},
-		{
-			n:    math.MaxUint64,
-			want: []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01},
-		},
-	}
-
-	for _, c := range cases {
-		b := new(bytes.Buffer)
-		n, err := WriteVarUint64(b, c.n)
-		if c.wantErr != err {
-			t.Errorf("WriteVarUint64(%d): err %v, want %v", c.n, err, c.wantErr)
-			continue
-		}
-		if c.wantErr != nil {
-			continue
-		}
-		if n != len(c.want) {
-			t.Errorf("WriteVarUint64(%d): wrote %d byte(s), want %d", c.n, n, len(c.want))
-		}
-		if !bytes.Equal(c.want, b.Bytes()) {
-			t.Errorf("WriteVarUint64(%d): got %x, want %x", c.n, b.Bytes(), c.want)
-		}
-		b = bytes.NewBuffer(b.Bytes())
-		v, n, err := ReadVarUint64(b)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if n != len(c.want) {
-			t.Errorf("ReadVarUint64 [c.n = %d] got %d bytes, want %d", c.n, n, len(c.want))
-		}
-		if uint64(v) != c.n {
-			t.Errorf("ReadVarUint64 got %d, want %d", v, c.n)
-		}
-	}
+func BenchmarkReadVarString(b *testing.B) {
+	data := []byte{10, 11, 12}
+	r := bytes.NewReader(data)
+	ReadVarString(r)
 }
 
-func TestVarInt64(t *testing.T) {
-	cases := []struct {
-		n       int64
-		want    []byte
-		wantErr error
-	}{
-		{
-			n:    0,
-			want: []byte{0},
-		},
-		{
-			n:    500,
-			want: []byte{0xe8, 0x07},
-		},
-		{
-			n:    math.MaxInt64,
-			want: []byte{0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01},
-		},
-	}
-
-	for _, c := range cases {
-		b := new(bytes.Buffer)
-		n, err := WriteVarInt64(b, c.n)
-		if c.wantErr != err {
-			t.Errorf("WriteVarInt64(%d): err %v, want %v", c.n, err, c.wantErr)
-			continue
-		}
-		if c.wantErr != nil {
-			continue
-		}
-		if n != len(c.want) {
-			t.Errorf("WriteVarInt64(%d): wrote %d byte(s), want %d", c.n, n, len(c.want))
-		}
-		if !bytes.Equal(c.want, b.Bytes()) {
-			t.Errorf("WriteVarInt64(%d): got %x, want %x", c.n, b.Bytes(), c.want)
-		}
-		b = bytes.NewBuffer(b.Bytes())
-		v, n, err := ReadVarInt64(b)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if n != len(c.want) {
-			t.Errorf("ReadVarInt64 [c.n = %d] got %d bytes, want %d", c.n, n, len(c.want))
-		}
-		if int64(v) != c.n {
-			t.Errorf("ReadVarInt64 got %d, want %d", v, c.n)
-		}
-	}
-}
-
-func TestVarBytes(t *testing.T) {
-	s := []byte{10, 11, 12}
+func TestSerialize(t *testing.T) {
 	b := new(bytes.Buffer)
-	_, err := WriteVarBytes(b, s)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := []byte{3, 10, 11, 12}
-	if !bytes.Equal(b.Bytes(), want) {
-		t.Errorf("got %x, want %x", b.Bytes(), want)
-	}
-	b = bytes.NewBuffer(want)
-	s, _, err = ReadVarBytes(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want = []byte{10, 11, 12}
-	if !bytes.Equal(s, want) {
-		t.Errorf("got %x, expected %x", s, want)
-	}
-}
+	a3 := uint8(100)
+	a4 := uint16(65535)
+	a5 := uint32(4294967295)
+	a6 := uint64(18446744073709551615)
+	a7 := uint64(18446744073709551615)
+	a8 := []byte{10, 11, 12}
+	a9 := "hello onchain."
 
-func TestVarString(t *testing.T) {
-	s := "hello"
-	b := new(bytes.Buffer)
-	_, err := WriteVarString(b, s)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := []byte{5, 104, 101, 108, 108, 111}
-	if !bytes.Equal(b.Bytes(), want) {
-		t.Errorf("got %x, want %x", b.Bytes(), want)
-	}
-	b = bytes.NewBuffer(want)
-	s, _, err = ReadVarstring(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want2 := "hello"
-	if !strings.EqualFold(s, want2) {
-		t.Errorf("got %x, expected %x", s, want2)
-	}
+	WriteVarUint(b, uint64(a3))
+	WriteVarUint(b, uint64(a4))
+	WriteVarUint(b, uint64(a5))
+	WriteVarUint(b, uint64(a6))
+	WriteVarUint(b, uint64(a7))
+	WriteVarBytes(b, a8)
+	WriteVarString(b, a9)
 
-}
+	fmt.Println(ReadVarUint(b, math.MaxUint64))
+	fmt.Println(ReadVarUint(b, math.MaxUint64))
+	fmt.Println(ReadVarUint(b, math.MaxUint64))
+	fmt.Println(ReadVarUint(b, math.MaxUint64))
+	fmt.Println(ReadVarUint(b, math.MaxUint32))
+	fmt.Println(ReadVarBytes(b))
+	fmt.Println(ReadVarString(b))
 
-func TestEmptyVarBytes(t *testing.T) {
-	s := []byte{}
-	b := new(bytes.Buffer)
-	_, err := WriteVarBytes(b, s)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := []byte{0x00}
-	if !bytes.Equal(b.Bytes(), want) {
-		t.Errorf("got %x, want %x", b.Bytes(), want)
-	}
-
-	b = bytes.NewBuffer(want)
-	s, _, err = ReadVarBytes(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want = nil // we deliberately return nil for empty strings to avoid unnecessary byteslice allocation
-	if !bytes.Equal(s, want) {
-		t.Errorf("got %x, expected %x", s, want)
-	}
 }
