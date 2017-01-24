d64cf3bb393dbf5ae41be61dd781e092a3fa9640
Li Jun <lijun@onchain.com>
2017-01-24 12:33:58 +0800

    Branch: Contract/Validation Functionalities
    Sync up Uint256

--- common/uint256.go
+++ common/uint256.go
@@ -2,21 +2,36 @@ package common
 
 import (
 	"io"
+	"bytes"
+	"encoding/binary"
+	_ "fmt"
 )
 
 type Uint256 [32]uint8
 
-func NewUint256(value []byte) *Uint256{
-	//TODO: NewUint256
-	return nil
-}
+func (u *Uint256) Serialize(w io.Writer) (int,error) {
+	b_buf := bytes.NewBuffer([]byte{})
+	binary.Write(b_buf, binary.LittleEndian, u)
 
-func (u *Uint256) Serialize(w io.Writer) {
-	//TODO: implement Uint256.serialize
+	len, err := w.Write( b_buf.Bytes() )
+
+	if err != nil {
+		return 0, err
+	}
+
+	return len, nil
 }
 
 func (u *Uint256) Deserialize(r io.Reader) error {
-	//TODO：Uint256 Deserialize
+	p := make([]byte, 32)
+	n, err := r.Read(p)
+
+	if n <= 0 || err != nil {
+		return err
+	}
+
+	b_buf := bytes.NewBuffer(p)
+	binary.Read(b_buf, binary.LittleEndian, u)
 
 	return nil
 }
