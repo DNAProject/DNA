f04d39ff507e640256e23308c1541f35101df9d4
zhengq1 <ljqlyy@gmail.com>
2017-01-26 13:30:28 +0800

    modified Store package, modified common package

--- common/Fixed64.go
+++ common/Fixed64.go
@@ -1,27 +1,72 @@
 package common
 
 import (
+	"bytes"
 	"io"
+	"strconv"
+	"encoding/binary"
 )
 
 //the 64 bit fixed-point number, precise 10^-8
-type Fixed64  struct {
-	//TODO: implement Fixed64 type
-}
+type Fixed64 int64
 
-
-func (f *Fixed64) Serialize(w io.Writer) {
-	//TODO: implement Fixed64.serialize
-}
-
-func (f *Fixed64) Deserialize(r io.Reader) error {
-	//TODO：Fixed64 Deserialize
+func (f *Fixed64) Serialize(w io.Writer) error {
+	err := binary.Write(w, binary.LittleEndian, int64(*f))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func (f *Fixed64) GetData() int64 {
-	//TODO: implement Fixed64.GetData
+func (f *Fixed64) Deserialize(r io.Reader) error {
+	p := make([]byte, 8)
+	n, err := r.Read(p)
+	if n <= 0 || err != nil {
+		return err
+	}
+	b_buf := bytes.NewBuffer(p)
+	var x int64
+	err = binary.Read(b_buf, binary.LittleEndian, &x)
+	if err != nil {
+		return err
+	}
 
-	return 0
+	*f = Fixed64(x)
+
+	return nil
+/*
+	var x int64
+	err := binary.Read(r, binary.LittleEndian, &x)
+	if err != nil {
+		return err
+	}
+
+	*f = Fixed64(x)
+*/
+	return nil
+}
+
+func (f Fixed64) GetData() int64 {
+	return int64(f)
+}
+
+func (f Fixed64) String() string {
+	var buffer bytes.Buffer
+	value := int64(f)
+	if value < 0 {
+		buffer.WriteRune('-')
+		value = -value
+	}
+	buffer.WriteString(strconv.FormatInt(value/100000000, 10))
+	value %= 100000000
+	if value > 0 {
+		buffer.WriteRune('.')
+		s := strconv.FormatInt(value, 10)
+		for i := len(s); i < 8; i++ {
+			buffer.WriteRune('0')
+		}
+		buffer.WriteString(s)
+	}
+	return buffer.String()
 }
--- common/uint160.go
+++ common/uint160.go
@@ -2,19 +2,60 @@ package common
 
 import (
 	"io"
+	"bytes"
+	"encoding/binary"
 )
 
-type Uint160  struct {
-	//TODO: implement Uint160 type
+type Uint160 [20]uint8
 
+func (u *Uint160) CompareTo( o Uint160 ) int {
+	x := u.ToArray()
+	y := o.ToArray()
+
+	for i:=len(x)-1; i>=0; i-- {
+		if ( x[i] > y[i] ) {
+			return 1
+		}
+		if ( x[i] < y[i] ) {
+			return -1
+		}
+	}
+
+	return 0
 }
 
+func (u *Uint160) ToArray() []byte {
+	var x []byte = make([]byte,32)
+	for i:=0; i<32; i++ {
+		x[i] = byte(u[i])
+	}
 
-func (u *Uint160) Serialize(w io.Writer) {
-	//TODO: implement Uint160.serialize
+	return x
+}
+
+func (u *Uint160) Serialize(w io.Writer) (int,error) {
+	b_buf := bytes.NewBuffer([]byte{})
+	binary.Write(b_buf, binary.LittleEndian, u)
+
+	len, err := w.Write( b_buf.Bytes() )
+
+	if err != nil {
+		return 0, err
+	}
+
+	return len, nil
 }
 
 func (f *Uint160) Deserialize(r io.Reader) error {
-	//TODO：Uint160 Deserialize
+	p := make([]byte, 20)
+	n, err := r.Read(p)
+
+	if n <= 0 || err != nil {
+		return err
+	}
+
+	b_buf := bytes.NewBuffer(p)
+	binary.Read(b_buf, binary.LittleEndian, f)
+
 	return nil
 }
