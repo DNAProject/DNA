31a8b3d33d8ed372dfefb350ea3cb51b699b306b
Yanbo Li <dreamfly281@gmail.com>
2017-02-16 13:59:30 +0800

    Implement the net module interface and node period update
    Implement the net ineterface Xmit, Transaction pool and node
    infomation update to get inventory
    
    Reformat the code with the go fmt command to make code style clear
    
    Signed-off-by: Yanbo Li <dreamfly281@gmail.com>

--- core/transaction/transaction.go
+++ core/transaction/transaction.go
@@ -10,8 +10,6 @@ import (
 	"crypto/sha256"
 	"errors"
 	"io"
-	msg "GoOnchain/net/message"
-
 )
 
 //for different transaction types with different payload format
@@ -283,8 +281,8 @@ func (tx *Transaction) SetHash(hash Uint256) {
 	tx.hash = &hash
 }
 
-func (tx *Transaction) Type() msg.InventoryType{
-	return msg.Transaction
+func (tx *Transaction) Type() InventoryType{
+	return TRANSACTION
 }
 
 func (tx *Transaction) Verify() error {
