a3f7163e902740afc4e817cea112939704fedb23
Ziyan Zhou <zhouziyan@hotmail.com>
2017-04-14 15:34:49 +0800

    Fix the nodectl can't show transactions issue
    root cause: did not allocated memory for Transaction Program
    solution: allocate memory for Transaction Program before assign value to it
    
    Signed-off-by: Ziyan Zhou <zhouziyan@hotmail.com>

--- net/httpjsonrpc/interfaces.go
+++ net/httpjsonrpc/interfaces.go
@@ -68,7 +68,9 @@ func getBlock(req *http.Request, cmd map[string]interface{}) map[string]interfac
 		trans[i].UTXOInputs = block.Transactions[i].UTXOInputs
 		trans[i].BalanceInputs = block.Transactions[i].BalanceInputs
 		trans[i].Outputs = block.Transactions[i].Outputs
+		trans[i].Programs = make([]*ProgramInfo, len(block.Transactions[i].Programs))
 		for n := 0; n < len(block.Transactions[i].Programs); n++ {
+			trans[i].Programs[n] = new(ProgramInfo)
 			trans[i].Programs[n].Code = ToHexString(block.Transactions[i].Programs[n].Code)
 			trans[i].Programs[n].Parameter = ToHexString(block.Transactions[i].Programs[n].Parameter)
 		}
