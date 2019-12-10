ontio/ontology

fix block fetch (#1134)
* get block from Chainstore when processing BlockFetchMsg

* fix blockpool testcase

* clean bookkeepers after test

consensus/vbft/block_pool_test.go
@@ -19,11 +19,15 @@
 package vbft
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
+	"github.com/ontio/ontology-crypto/keypair"
 	"github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/consensus/vbft/config"
 	"github.com/ontio/ontology/core/ledger"
+	"github.com/ontio/ontology/core/signature"
 	"github.com/ontio/ontology/core/types"
 )
 
@@ -42,6 +46,11 @@ func buildTestBlock(t *testing.T, lastBlock *types.Block, lgr *ledger.Ledger) (*
 	txRoot := common.ComputeMerkleRoot(txHash)
 	blockRoot := lgr.GetBlockRootWithNewTxRoots(lastBlock.Header.Height, []common.Uint256{lastBlock.Header.TransactionsRoot, txRoot})
 
+	consensusPayload, err := json.Marshal(&vconfig.VbftBlockInfo{})
+	if err != nil {
+		t.Fatalf("failed to build consensus payload: %s", err)
+	}
+
 	blkHeader := &types.Header{
 		PrevBlockHash:    lastBlock.Header.Hash(),
 		TransactionsRoot: txRoot,
@@ -49,8 +58,23 @@ func buildTestBlock(t *testing.T, lastBlock *types.Block, lgr *ledger.Ledger) (*
 		Timestamp:        timestamp,
 		Height:           lastBlock.Header.Height + 1,
 		ConsensusData:    common.GetNonce(),
-		ConsensusPayload: nil,
+		ConsensusPayload: consensusPayload,
+		Bookkeepers:      make([]keypair.PublicKey, 0),
+		SigData:          make([][]byte, 0),
 	}
+
+	// add sigs
+	hash := blkHeader.Hash()
+	for i := 0; i < 5; i++ {
+		acc := testBookkeeperAccounts[i]
+		sig, err := signature.Sign(acc, hash[:])
+		if err != nil {
+			t.Fatalf("bookkeeper %d sign block: %s", i, err)
+		}
+		blkHeader.Bookkeepers = append(blkHeader.Bookkeepers, acc.PublicKey)
+		blkHeader.SigData = append(blkHeader.SigData, sig)
+	}
+
 	blk := &types.Block{
 		Header:       blkHeader,
 		Transactions: txs,
@@ -65,6 +89,8 @@ func TestAddBlock(t *testing.T) {
 	if err != nil {
 		t.Errorf("buildTestBlockPool err:%s", err)
 	}
+	defer cleanTestChainStore()
+
 	lastBlock, _ := blockpool.getSealedBlock(0)
 	if lastBlock == nil {
 		t.Errorf("getblock err")

consensus/vbft/chain_store_test.go
@@ -19,15 +19,19 @@
 package vbft
 
 import (
+	"os"
+	"testing"
+
 	"github.com/ontio/ontology-crypto/keypair"
 	"github.com/ontio/ontology/account"
 	"github.com/ontio/ontology/common/config"
 	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/consensus/vbft/config"
 	"github.com/ontio/ontology/core/genesis"
 	"github.com/ontio/ontology/core/ledger"
-	"os"
-	"testing"
 )
+
+var testBookkeeperAccounts []*account.Account
 
 func newTestChainStore(t *testing.T) *ChainStore {
 	log.InitLog(log.InfoLog, log.Stdout)
@@ -41,20 +45,29 @@ func newTestChainStore(t *testing.T) *ChainStore {
 	if err != nil {
 		t.Fatalf("NewLedger error %s", err)
 	}
-	acc1 := account.NewAccount("")
-	acc2 := account.NewAccount("")
-	acc3 := account.NewAccount("")
-	acc4 := account.NewAccount("")
-	acc5 := account.NewAccount("")
-	acc6 := account.NewAccount("")
-	acc7 := account.NewAccount("")
 
-	bookkeepers := []keypair.PublicKey{acc1.PublicKey, acc2.PublicKey, acc3.PublicKey, acc4.PublicKey, acc5.PublicKey, acc6.PublicKey, acc7.PublicKey}
+	var bookkeepers []keypair.PublicKey
+	if len(testBookkeeperAccounts) == 0 {
+		for i := 0; i < 7; i++ {
+			acc := account.NewAccount("")
+			testBookkeeperAccounts = append(testBookkeeperAccounts, acc)
+			bookkeepers = append(bookkeepers, acc.PublicKey)
+		}
+	}
+
 	genesisConfig := config.DefConfig.Genesis
+
+	// update peers in genesis
+	for i, p := range genesisConfig.VBFT.Peers {
+		if i > 0 && i <= len(testBookkeeperAccounts) {
+			p.PeerPubkey = vconfig.PubkeyID(testBookkeeperAccounts[i-1].PublicKey)
+		}
+	}
 	block, err := genesis.BuildGenesisBlock(bookkeepers, genesisConfig)
 	if err != nil {
 		t.Fatalf("BuildGenesisBlock error %s", err)
 	}
+
 	err = db.Init(bookkeepers, block)
 	if err != nil {
 		t.Fatalf("InitLedgerStoreWithGenesisBlock error %s", err)
@@ -68,6 +81,7 @@ func newTestChainStore(t *testing.T) *ChainStore {
 
 func cleanTestChainStore() {
 	os.RemoveAll(config.DEFAULT_DATA_DIR)
+	testBookkeeperAccounts = make([]*account.Account, 0)
 }
 
 func TestGetChainedBlockNum(t *testing.T) {

DNAProject/DNA

consensus/vbft/block_pool_test.go
@@ -22,12 +22,16 @@
 package vbft
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
 	"github.com/DNAProject/DNA/common"
+	"github.com/DNAProject/DNA/consensus/vbft/config"
 	"github.com/DNAProject/DNA/core/ledger"
+	"github.com/DNAProject/DNA/core/signature"
 	"github.com/DNAProject/DNA/core/types"
+	"github.com/ontio/ontology-crypto/keypair"
 )
 
 func buildTestBlockPool(t *testing.T) (*BlockPool, error) {
@@ -45,6 +49,11 @@ func buildTestBlock(t *testing.T, lastBlock *types.Block, lgr *ledger.Ledger) (*
 	txRoot := common.ComputeMerkleRoot(txHash)
 	blockRoot := lgr.GetBlockRootWithNewTxRoots(lastBlock.Header.Height, []common.Uint256{lastBlock.Header.TransactionsRoot, txRoot})
 
+	consensusPayload, err := json.Marshal(&vconfig.VbftBlockInfo{})
+	if err != nil {
+		t.Fatalf("failed to build consensus payload: %s", err)
+	}
+
 	blkHeader := &types.Header{
 		PrevBlockHash:    lastBlock.Header.Hash(),
 		TransactionsRoot: txRoot,
@@ -52,8 +61,23 @@ func buildTestBlock(t *testing.T, lastBlock *types.Block, lgr *ledger.Ledger) (*
 		Timestamp:        timestamp,
 		Height:           lastBlock.Header.Height + 1,
 		ConsensusData:    common.GetNonce(),
-		ConsensusPayload: nil,
+		ConsensusPayload: consensusPayload,
+		Bookkeepers:      make([]keypair.PublicKey, 0),
+		SigData:          make([][]byte, 0),
 	}
+
+	// add sigs
+	hash := blkHeader.Hash()
+	for i := 0; i < 5; i++ {
+		acc := testBookkeeperAccounts[i]
+		sig, err := signature.Sign(acc, hash[:])
+		if err != nil {
+			t.Fatalf("bookkeeper %d sign block: %s", i, err)
+		}
+		blkHeader.Bookkeepers = append(blkHeader.Bookkeepers, acc.PublicKey)
+		blkHeader.SigData = append(blkHeader.SigData, sig)
+	}
+
 	blk := &types.Block{
 		Header:       blkHeader,
 		Transactions: txs,
@@ -68,6 +92,8 @@ func TestAddBlock(t *testing.T) {
 	if err != nil {
 		t.Errorf("buildTestBlockPool err:%s", err)
 	}
+	defer cleanTestChainStore()
+
 	lastBlock, _ := blockpool.getSealedBlock(0)
 	if lastBlock == nil {
 		t.Errorf("getblock err")

consensus/vbft/chain_store_test.go
@@ -22,15 +22,19 @@
 package vbft
 
 import (
+	"os"
+	"testing"
+
 	"github.com/DNAProject/DNA/account"
 	"github.com/DNAProject/DNA/common/config"
 	"github.com/DNAProject/DNA/common/log"
+	"github.com/DNAProject/DNA/consensus/vbft/config"
 	"github.com/DNAProject/DNA/core/genesis"
 	"github.com/DNAProject/DNA/core/ledger"
 	"github.com/ontio/ontology-crypto/keypair"
-	"os"
-	"testing"
 )
+
+var testBookkeeperAccounts []*account.Account
 
 func newTestChainStore(t *testing.T) *ChainStore {
 	log.InitLog(log.InfoLog, log.Stdout)
@@ -44,20 +48,29 @@ func newTestChainStore(t *testing.T) *ChainStore {
 	if err != nil {
 		t.Fatalf("NewLedger error %s", err)
 	}
-	acc1 := account.NewAccount("")
-	acc2 := account.NewAccount("")
-	acc3 := account.NewAccount("")
-	acc4 := account.NewAccount("")
-	acc5 := account.NewAccount("")
-	acc6 := account.NewAccount("")
-	acc7 := account.NewAccount("")
 
-	bookkeepers := []keypair.PublicKey{acc1.PublicKey, acc2.PublicKey, acc3.PublicKey, acc4.PublicKey, acc5.PublicKey, acc6.PublicKey, acc7.PublicKey}
+	var bookkeepers []keypair.PublicKey
+	if len(testBookkeeperAccounts) == 0 {
+		for i := 0; i < 7; i++ {
+			acc := account.NewAccount("")
+			testBookkeeperAccounts = append(testBookkeeperAccounts, acc)
+			bookkeepers = append(bookkeepers, acc.PublicKey)
+		}
+	}
+
 	genesisConfig := config.DefConfig.Genesis
+
+	// update peers in genesis
+	for i, p := range genesisConfig.VBFT.Peers {
+		if i > 0 && i <= len(testBookkeeperAccounts) {
+			p.PeerPubkey = vconfig.PubkeyID(testBookkeeperAccounts[i-1].PublicKey)
+		}
+	}
 	block, err := genesis.BuildGenesisBlock(bookkeepers, genesisConfig)
 	if err != nil {
 		t.Fatalf("BuildGenesisBlock error %s", err)
 	}
+
 	err = db.Init(bookkeepers, block)
 	if err != nil {
 		t.Fatalf("InitLedgerStoreWithGenesisBlock error %s", err)
@@ -71,6 +84,7 @@ func newTestChainStore(t *testing.T) *ChainStore {
 
 func cleanTestChainStore() {
 	os.RemoveAll(config.DEFAULT_DATA_DIR)
+	testBookkeeperAccounts = make([]*account.Account, 0)
 }
 
 func TestGetChainedBlockNum(t *testing.T) {