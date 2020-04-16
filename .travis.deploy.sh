510a8c0d5aa18169042755aea6a79ebf66f45ac0
Edmond <conghonglei@gmail.com>
2020-04-16 09:50:25 +0800

    add travis deploy script (#440)

---
+++ .travis.deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+VERSION=$(git describe --always --tags --long)
+PLATFORM=""
+
+if [[ ${TRAVIS_OS_NAME} == 'linux' ]]; then
+	PLATFORM="linux"
+elif [[ ${TRAVIS_OS_NAME} == 'osx' ]]; then
+	PLATFORM="darwin"
+else
+	PLATFORM="windows"
+	exit 1
+fi
+
+env GO111MODULE=on make DNA-${PLATFORM}
+
+set +x
+echo "dnaNode-${PLATFORM}-amd64 |" $(md5sum dnaNode-${PLATFORM}-amd64|cut -d ' ' -f1)
+
