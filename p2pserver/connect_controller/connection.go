rongyi/ontology

refactor peer id

p2pserver/connect_controller/connection.go
@@ -21,14 +21,14 @@ import (
 	"net"
 
 	"github.com/ontio/ontology/common/log"
-	"github.com/ontio/ontology/p2pserver/dht/kbucket"
+	"github.com/ontio/ontology/p2pserver/common"
 )
 
 // Conn is a net.Conn wrapper to do some clean up when Close.
 type Conn struct {
 	net.Conn
 	addr       string
-	kid        kbucket.KadId
+	kid        common.PeerId
 	boundIndex int
 	connectId  uint64
 	controller *ConnectController

p2pserver/dht/dht.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/p2pserver/common"
 	kb "github.com/ontio/ontology/p2pserver/dht/kbucket"
 )
 
@@ -35,7 +36,7 @@ var KValue = 20
 var AlphaValue = 3
 
 type DHT struct {
-	localKeyId *kb.KadKeyId
+	localKeyId *common.PeerKeyId
 	birth      time.Time // When this peer started up
 
 	bucketSize int
@@ -54,14 +55,14 @@ func (dht *DHT) RouteTable() *kb.RouteTable {
 // NewDHT creates a new DHT with the specified host and options.
 func NewDHT() *DHT {
 	bucketSize := KValue
-	keyId := kb.RandKadKeyId()
+	keyId := common.RandPeerKeyId()
 	rt := kb.NewRoutingTable(bucketSize, keyId.Id)
 
-	rt.PeerAdded = func(p kb.KadId) {
+	rt.PeerAdded = func(p common.PeerId) {
 		log.Debugf("dht: peer: %d added to dht", p)
 	}
 
-	rt.PeerRemoved = func(p kb.KadId) {
+	rt.PeerRemoved = func(p common.PeerId) {
 		log.Debugf("dht: peer: %d removed from dht", p)
 	}
 
@@ -78,22 +79,22 @@ func NewDHT() *DHT {
 
 // Update signals the routeTable to Update its last-seen status
 // on the given peer.
-func (dht *DHT) Update(peer kb.KadId) bool {
+func (dht *DHT) Update(peer common.PeerId) bool {
 	err := dht.routeTable.Update(peer)
 	return err == nil
 }
 
-func (dht *DHT) Remove(peer kb.KadId) {
+func (dht *DHT) Remove(peer common.PeerId) {
 	dht.routeTable.Remove(peer)
 }
 
-func (dht *DHT) GetKadKeyId() *kb.KadKeyId {
+func (dht *DHT) GetPeerKeyId() *common.PeerKeyId {
 	return dht.localKeyId
 }
 
-func (dht *DHT) BetterPeers(id kb.KadId, count int) []kb.KadId {
+func (dht *DHT) BetterPeers(id common.PeerId, count int) []common.PeerId {
 	closer := dht.routeTable.NearestPeers(id, count)
-	filtered := make([]kb.KadId, 0, len(closer))
+	filtered := make([]common.PeerId, 0, len(closer))
 	// don't include self and target id
 	for _, curID := range closer {
 		if curID == dht.localKeyId.Id {

p2pserver/message/types/consensus_payload.go
@@ -26,6 +26,7 @@ import (
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/core/signature"
 	"github.com/ontio/ontology/errors"
+	common2 "github.com/ontio/ontology/p2pserver/common"
 )
 
 type ConsensusPayload struct {
@@ -37,7 +38,7 @@ type ConsensusPayload struct {
 	Data            []byte
 	Owner           keypair.PublicKey
 	Signature       []byte
-	PeerId          uint64
+	PeerId          common2.PeerId
 	hash            common.Uint256
 }
 

DNAProject/DNA

p2pserver/connect_controller/connection.go
@@ -21,17 +21,16 @@
 package connect_controller
 
 import (
-	"net"
-
 	"github.com/DNAProject/DNA/common/log"
-	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
+	"github.com/DNAProject/DNA/p2pserver/common"
+	"net"
 )
 
 // Conn is a net.Conn wrapper to do some clean up when Close.
 type Conn struct {
 	net.Conn
 	addr       string
-	kid        kbucket.KadId
+	kid        common.PeerId
 	boundIndex int
 	connectId  uint64
 	controller *ConnectController

p2pserver/dht/dht.go
@@ -22,10 +22,10 @@
 package dht
 
 import (
-	"time"
-
 	"github.com/DNAProject/DNA/common/log"
+	"github.com/DNAProject/DNA/p2pserver/common"
 	kb "github.com/DNAProject/DNA/p2pserver/dht/kbucket"
+	"time"
 )
 
 // Pool size is the number of nodes used for group find/set RPC calls
@@ -38,7 +38,7 @@ var KValue = 20
 var AlphaValue = 3
 
 type DHT struct {
-	localKeyId *kb.KadKeyId
+	localKeyId *common.PeerKeyId
 	birth      time.Time // When this peer started up
 
 	bucketSize int
@@ -57,14 +57,14 @@ func (dht *DHT) RouteTable() *kb.RouteTable {
 // NewDHT creates a new DHT with the specified host and options.
 func NewDHT() *DHT {
 	bucketSize := KValue
-	keyId := kb.RandKadKeyId()
+	keyId := common.RandPeerKeyId()
 	rt := kb.NewRoutingTable(bucketSize, keyId.Id)
 
-	rt.PeerAdded = func(p kb.KadId) {
+	rt.PeerAdded = func(p common.PeerId) {
 		log.Debugf("dht: peer: %d added to dht", p)
 	}
 
-	rt.PeerRemoved = func(p kb.KadId) {
+	rt.PeerRemoved = func(p common.PeerId) {
 		log.Debugf("dht: peer: %d removed from dht", p)
 	}
 
@@ -81,22 +81,22 @@ func NewDHT() *DHT {
 
 // Update signals the routeTable to Update its last-seen status
 // on the given peer.
-func (dht *DHT) Update(peer kb.KadId) bool {
+func (dht *DHT) Update(peer common.PeerId) bool {
 	err := dht.routeTable.Update(peer)
 	return err == nil
 }
 
-func (dht *DHT) Remove(peer kb.KadId) {
+func (dht *DHT) Remove(peer common.PeerId) {
 	dht.routeTable.Remove(peer)
 }
 
-func (dht *DHT) GetKadKeyId() *kb.KadKeyId {
+func (dht *DHT) GetPeerKeyId() *common.PeerKeyId {
 	return dht.localKeyId
 }
 
-func (dht *DHT) BetterPeers(id kb.KadId, count int) []kb.KadId {
+func (dht *DHT) BetterPeers(id common.PeerId, count int) []common.PeerId {
 	closer := dht.routeTable.NearestPeers(id, count)
-	filtered := make([]kb.KadId, 0, len(closer))
+	filtered := make([]common.PeerId, 0, len(closer))
 	// don't include self and target id
 	for _, curID := range closer {
 		if curID == dht.localKeyId.Id {

p2pserver/message/types/consensus_payload.go
@@ -28,6 +28,7 @@ import (
 	"github.com/DNAProject/DNA/common"
 	"github.com/DNAProject/DNA/core/signature"
 	"github.com/DNAProject/DNA/errors"
+	common2 "github.com/DNAProject/DNA/p2pserver/common"
 	"github.com/ontio/ontology-crypto/keypair"
 )
 
@@ -40,7 +41,7 @@ type ConsensusPayload struct {
 	Data            []byte
 	Owner           keypair.PublicKey
 	Signature       []byte
-	PeerId          uint64
+	PeerId          common2.PeerId
 	hash            common.Uint256
 }
 