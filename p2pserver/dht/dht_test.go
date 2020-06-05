rongyi/ontology

refactor heartbeat
* refactor heartbeat

* fix pinghandle

* persist recent peers

* use networkmagic

* add license

* add stop

p2pserver/dht/dht_test.go
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dht
+
+import (
+	"fmt"
+	"github.com/ontio/ontology/p2pserver/common"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestNewDHT(t *testing.T) {
+	id := common.RandPeerKeyId()
+	dht := NewDHT(id.Id)
+	assert.NotNil(t, dht)
+	assert.Equal(t, dht.AutoRefresh, true)
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+func TestDHT_Update(t *testing.T) {
+	common.Difficulty = 8
+	for i := 0; i < 10; i++ {
+		id := common.RandPeerKeyId()
+		dht := NewDHT(id.Id)
+		local := dht.localId
+		prefix := rand.Int31n(15)
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		assert.True(t, boo)
+		if prefix == 0 {
+			continue
+		}
+		kids := dht.BetterPeers(dht.localId, int(prefix))
+		assert.Equal(t, len(kids), 1)
+		assert.Equal(t, kids[0], kid)
+	}
+}
+
+func TestDHT_Remove(t *testing.T) {
+	common.Difficulty = 8
+	for i := 0; i < 100; i++ {
+		id := common.RandPeerKeyId()
+		dht := NewDHT(id.Id)
+		local := dht.localId
+		prefix := rand.Int31n(15)
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		assert.True(t, boo)
+		kids := dht.BetterPeers(dht.localId, 1)
+		assert.Equal(t, len(kids), 1)
+		assert.Equal(t, kids[0], kid)
+		dht.Remove(kid)
+		kids = dht.BetterPeers(dht.localId, int(prefix))
+		assert.Equal(t, len(kids), 0)
+	}
+
+}
+
+func TestDHT_BetterPeers(t *testing.T) {
+	id := common.RandPeerKeyId()
+	dht := NewDHT(id.Id)
+	local := dht.localId
+	rand.Seed(time.Now().Unix())
+	prefix := rand.Int31n(15)
+	for i := 0; i < 15; i++ {
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		if !boo {
+			fmt.Println(boo, prefix)
+		}
+		assert.True(t, boo)
+	}
+	kids := dht.BetterPeers(dht.localId, 3)
+	assert.Equal(t, len(kids), 3)
+}

p2pserver/protocols/heatbeat/heartbeat.go
@@ -1,0 +1,124 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package heatbeat
+
+import (
+	"time"
+
+	"github.com/ontio/ontology/common/config"
+	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/core/ledger"
+	"github.com/ontio/ontology/p2pserver/common"
+	"github.com/ontio/ontology/p2pserver/message/msg_pack"
+	"github.com/ontio/ontology/p2pserver/message/types"
+	"github.com/ontio/ontology/p2pserver/net/protocol"
+	"github.com/ontio/ontology/p2pserver/peer"
+)
+
+type HeartBeat struct {
+	net    p2p.P2P
+	id     common.PeerId
+	quit   chan bool
+	ledger *ledger.Ledger //ledger
+}
+
+func NewHeartBeat(net p2p.P2P, ld *ledger.Ledger) *HeartBeat {
+	return &HeartBeat{
+		id:     net.GetID(),
+		net:    net,
+		quit:   make(chan bool),
+		ledger: ld,
+	}
+}
+
+func (self *HeartBeat) Start() {
+	go self.heartBeatService()
+}
+
+func (self *HeartBeat) Stop() {
+	close(self.quit)
+}
+func (this *HeartBeat) heartBeatService() {
+	var periodTime uint
+	periodTime = config.DEFAULT_GEN_BLOCK_TIME / common.UPDATE_RATE_PER_BLOCK
+	t := time.NewTicker(time.Second * (time.Duration(periodTime)))
+
+	for {
+		select {
+		case <-t.C:
+			this.ping()
+			this.timeout()
+		case <-this.quit:
+			t.Stop()
+			return
+		}
+	}
+}
+
+func (this *HeartBeat) ping() {
+	peers := this.net.GetNeighbors()
+	this.pingTo(peers)
+}
+
+//pings send pkgs to get pong msg from others
+func (this *HeartBeat) pingTo(peers []*peer.Peer) {
+	for _, p := range peers {
+		if p.GetState() == common.ESTABLISH {
+			height := this.ledger.GetCurrentBlockHeight()
+			ping := msgpack.NewPingMsg(uint64(height))
+			go this.net.Send(p, ping)
+		}
+	}
+}
+
+//timeout trace whether some peer be long time no response
+func (this *HeartBeat) timeout() {
+	peers := this.net.GetNeighbors()
+	var periodTime uint
+	periodTime = config.DEFAULT_GEN_BLOCK_TIME / common.UPDATE_RATE_PER_BLOCK
+	for _, p := range peers {
+		if p.GetState() == common.ESTABLISH {
+			t := p.GetContactTime()
+			if t.Before(time.Now().Add(-1 * time.Second *
+				time.Duration(periodTime) * common.KEEPALIVE_TIMEOUT)) {
+				log.Warnf("[p2p]keep alive timeout!!!lost remote peer %d - %s from %s", p.GetID(), p.Link.GetAddr(), t.String())
+				p.Close()
+			}
+		}
+	}
+}
+
+func (this *HeartBeat) PingHandle(ctx *p2p.Context, ping *types.Ping) {
+	remotePeer := ctx.Sender()
+	remotePeer.SetHeight(ping.Height)
+	p2p := ctx.Network()
+
+	height := ledger.DefLedger.GetCurrentBlockHeight()
+	p2p.SetHeight(uint64(height))
+	msg := msgpack.NewPongMsg(uint64(height))
+
+	err := remotePeer.Send(msg)
+	if err != nil {
+		log.Warn(err)
+	}
+}
+
+func (this *HeartBeat) PongHandle(ctx *p2p.Context, pong *types.Pong) {
+	remotePeer := ctx.Network()
+	remotePeer.SetHeight(pong.Height)
+}

p2pserver/protocols/msg_handler.go
@@ -24,7 +24,7 @@ import (
 	"net"
 	"strconv"
 
-	lru "github.com/hashicorp/golang-lru"
+	"github.com/hashicorp/golang-lru"
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/config"
 	"github.com/ontio/ontology/common/log"
@@ -32,11 +32,13 @@ import (
 	"github.com/ontio/ontology/core/types"
 	actor "github.com/ontio/ontology/p2pserver/actor/req"
 	msgCommon "github.com/ontio/ontology/p2pserver/common"
-	msgpack "github.com/ontio/ontology/p2pserver/message/msg_pack"
+	"github.com/ontio/ontology/p2pserver/message/msg_pack"
 	msgTypes "github.com/ontio/ontology/p2pserver/message/types"
-	p2p "github.com/ontio/ontology/p2pserver/net/protocol"
+	"github.com/ontio/ontology/p2pserver/net/protocol"
 	"github.com/ontio/ontology/p2pserver/protocols/block_sync"
 	"github.com/ontio/ontology/p2pserver/protocols/discovery"
+	"github.com/ontio/ontology/p2pserver/protocols/heatbeat"
+	"github.com/ontio/ontology/p2pserver/protocols/recent_peers"
 	"github.com/ontio/ontology/p2pserver/protocols/reconnect"
 )
 
@@ -48,10 +50,12 @@ var respCache *lru.ARCCache
 var txCache, _ = lru.NewARC(msgCommon.MAX_TX_CACHE_SIZE)
 
 type MsgHandler struct {
-	blockSync *block_sync.BlockSyncMgr
-	reconnect *reconnect.ReconnectService
-	discovery *discovery.Discovery
-	ledger    *ledger.Ledger
+	blockSync                *block_sync.BlockSyncMgr
+	reconnect                *reconnect.ReconnectService
+	discovery                *discovery.Discovery
+	heatBeat                 *heatbeat.HeartBeat
+	persistRecentPeerService *recent_peers.PersistRecentPeerService
+	ledger                   *ledger.Ledger
 }
 
 func NewMsgHandler(ld *ledger.Ledger) *MsgHandler {
@@ -62,15 +66,21 @@ func (self *MsgHandler) start(net p2p.P2P) {
 	self.blockSync = block_sync.NewBlockSyncMgr(net, self.ledger)
 	self.reconnect = reconnect.NewReconectService(net)
 	self.discovery = discovery.NewDiscovery(net)
-
+	self.heatBeat = heatbeat.NewHeartBeat(net, self.ledger)
+	self.persistRecentPeerService = recent_peers.NewPersistRecentPeerService(net)
+	go self.persistRecentPeerService.Start()
 	go self.blockSync.Start()
 	go self.reconnect.Start()
 	go self.discovery.Start()
+	go self.heatBeat.Start()
 }
 
 func (self *MsgHandler) stop() {
 	self.blockSync.Close()
 	self.reconnect.Close()
+	self.discovery.Stop()
+	self.persistRecentPeerService.Stop()
+	self.heatBeat.Stop()
 }
 
 func (self *MsgHandler) HandleSystemMessage(net p2p.P2P, msg p2p.SystemMessage) {
@@ -81,10 +91,12 @@ func (self *MsgHandler) HandleSystemMessage(net p2p.P2P, msg p2p.SystemMessage) 
 		self.blockSync.OnAddNode(m.Info.Id)
 		self.reconnect.OnAddPeer(m.Info)
 		self.discovery.OnAddPeer(m.Info)
+		self.persistRecentPeerService.AddNodeAddr(m.Info.Addr + strconv.Itoa(int(m.Info.Port)))
 	case p2p.PeerDisConnected:
 		self.blockSync.OnDelNode(m.Info.Id)
 		self.reconnect.OnDelPeer(m.Info)
 		self.discovery.OnDelPeer(m.Info)
+		self.persistRecentPeerService.DelNodeAddr(m.Info.Addr + strconv.Itoa(int(m.Info.Port)))
 	case p2p.NetworkStop:
 		self.stop()
 	}
@@ -102,9 +114,9 @@ func (self *MsgHandler) HandlePeerMessage(ctx *p2p.Context, msg msgTypes.Message
 	case *msgTypes.HeadersReq:
 		HeadersReqHandle(ctx, m)
 	case *msgTypes.Ping:
-		PingHandle(ctx, m)
+		self.heatBeat.PingHandle(ctx, m)
 	case *msgTypes.Pong:
-		PongHandle(ctx, m)
+		self.heatBeat.PongHandle(ctx, m)
 	case *msgTypes.BlkHeader:
 		self.blockSync.OnHeaderReceive(ctx.Sender().GetID(), m.BlkHdr)
 	case *msgTypes.Block:
@@ -195,28 +207,6 @@ func HeadersReqHandle(ctx *p2p.Context, headersReq *msgTypes.HeadersReq) {
 		log.Warn(err)
 		return
 	}
-}
-
-//PingHandle handle ping msg from peer
-func PingHandle(ctx *p2p.Context, ping *msgTypes.Ping) {
-	remotePeer := ctx.Sender()
-	remotePeer.SetHeight(ping.Height)
-	p2p := ctx.Network()
-
-	height := ledger.DefLedger.GetCurrentBlockHeight()
-	p2p.SetHeight(uint64(height))
-	msg := msgpack.NewPongMsg(uint64(height))
-
-	err := remotePeer.Send(msg)
-	if err != nil {
-		log.Warn(err)
-	}
-}
-
-///PongHandle handle pong msg from peer
-func PongHandle(ctx *p2p.Context, pong *msgTypes.Pong) {
-	remotePeer := ctx.Network()
-	remotePeer.SetHeight(pong.Height)
 }
 
 // blockHandle handles the block message from peer

DNAProject/DNA

p2pserver/dht/dht_test.go
@@ -1,0 +1,98 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dht
+
+import (
+	"fmt"
+	"github.com/DNAProject/DNA/p2pserver/common"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestNewDHT(t *testing.T) {
+	id := common.RandPeerKeyId()
+	dht := NewDHT(id.Id)
+	assert.NotNil(t, dht)
+	assert.Equal(t, dht.AutoRefresh, true)
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+func TestDHT_Update(t *testing.T) {
+	common.Difficulty = 8
+	for i := 0; i < 10; i++ {
+		id := common.RandPeerKeyId()
+		dht := NewDHT(id.Id)
+		local := dht.localId
+		prefix := rand.Int31n(15)
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		assert.True(t, boo)
+		if prefix == 0 {
+			continue
+		}
+		kids := dht.BetterPeers(dht.localId, int(prefix))
+		assert.Equal(t, len(kids), 1)
+		assert.Equal(t, kids[0], kid)
+	}
+}
+
+func TestDHT_Remove(t *testing.T) {
+	common.Difficulty = 8
+	for i := 0; i < 100; i++ {
+		id := common.RandPeerKeyId()
+		dht := NewDHT(id.Id)
+		local := dht.localId
+		prefix := rand.Int31n(15)
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		assert.True(t, boo)
+		kids := dht.BetterPeers(dht.localId, 1)
+		assert.Equal(t, len(kids), 1)
+		assert.Equal(t, kids[0], kid)
+		dht.Remove(kid)
+		kids = dht.BetterPeers(dht.localId, int(prefix))
+		assert.Equal(t, len(kids), 0)
+	}
+
+}
+
+func TestDHT_BetterPeers(t *testing.T) {
+	id := common.RandPeerKeyId()
+	dht := NewDHT(id.Id)
+	local := dht.localId
+	rand.Seed(time.Now().Unix())
+	prefix := rand.Int31n(15)
+	for i := 0; i < 15; i++ {
+		kid := local.GenRandPeerId(uint(prefix))
+		boo := dht.Update(kid)
+		if !boo {
+			fmt.Println(boo, prefix)
+		}
+		assert.True(t, boo)
+	}
+	kids := dht.BetterPeers(dht.localId, 3)
+	assert.Equal(t, len(kids), 3)
+}

p2pserver/protocols/heatbeat/heartbeat.go
@@ -1,0 +1,127 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package heatbeat
+
+import (
+	"time"
+
+	"github.com/DNAProject/DNA/common/config"
+	"github.com/DNAProject/DNA/common/log"
+	"github.com/DNAProject/DNA/core/ledger"
+	"github.com/DNAProject/DNA/p2pserver/common"
+	"github.com/DNAProject/DNA/p2pserver/message/msg_pack"
+	"github.com/DNAProject/DNA/p2pserver/message/types"
+	"github.com/DNAProject/DNA/p2pserver/net/protocol"
+	"github.com/DNAProject/DNA/p2pserver/peer"
+)
+
+type HeartBeat struct {
+	net    p2p.P2P
+	id     common.PeerId
+	quit   chan bool
+	ledger *ledger.Ledger //ledger
+}
+
+func NewHeartBeat(net p2p.P2P, ld *ledger.Ledger) *HeartBeat {
+	return &HeartBeat{
+		id:     net.GetID(),
+		net:    net,
+		quit:   make(chan bool),
+		ledger: ld,
+	}
+}
+
+func (self *HeartBeat) Start() {
+	go self.heartBeatService()
+}
+
+func (self *HeartBeat) Stop() {
+	close(self.quit)
+}
+func (this *HeartBeat) heartBeatService() {
+	var periodTime uint
+	periodTime = config.DEFAULT_GEN_BLOCK_TIME / common.UPDATE_RATE_PER_BLOCK
+	t := time.NewTicker(time.Second * (time.Duration(periodTime)))
+
+	for {
+		select {
+		case <-t.C:
+			this.ping()
+			this.timeout()
+		case <-this.quit:
+			t.Stop()
+			return
+		}
+	}
+}
+
+func (this *HeartBeat) ping() {
+	peers := this.net.GetNeighbors()
+	this.pingTo(peers)
+}
+
+//pings send pkgs to get pong msg from others
+func (this *HeartBeat) pingTo(peers []*peer.Peer) {
+	for _, p := range peers {
+		if p.GetState() == common.ESTABLISH {
+			height := this.ledger.GetCurrentBlockHeight()
+			ping := msgpack.NewPingMsg(uint64(height))
+			go this.net.Send(p, ping)
+		}
+	}
+}
+
+//timeout trace whether some peer be long time no response
+func (this *HeartBeat) timeout() {
+	peers := this.net.GetNeighbors()
+	var periodTime uint
+	periodTime = config.DEFAULT_GEN_BLOCK_TIME / common.UPDATE_RATE_PER_BLOCK
+	for _, p := range peers {
+		if p.GetState() == common.ESTABLISH {
+			t := p.GetContactTime()
+			if t.Before(time.Now().Add(-1 * time.Second *
+				time.Duration(periodTime) * common.KEEPALIVE_TIMEOUT)) {
+				log.Warnf("[p2p]keep alive timeout!!!lost remote peer %d - %s from %s", p.GetID(), p.Link.GetAddr(), t.String())
+				p.Close()
+			}
+		}
+	}
+}
+
+func (this *HeartBeat) PingHandle(ctx *p2p.Context, ping *types.Ping) {
+	remotePeer := ctx.Sender()
+	remotePeer.SetHeight(ping.Height)
+	p2p := ctx.Network()
+
+	height := ledger.DefLedger.GetCurrentBlockHeight()
+	p2p.SetHeight(uint64(height))
+	msg := msgpack.NewPongMsg(uint64(height))
+
+	err := remotePeer.Send(msg)
+	if err != nil {
+		log.Warn(err)
+	}
+}
+
+func (this *HeartBeat) PongHandle(ctx *p2p.Context, pong *types.Pong) {
+	remotePeer := ctx.Network()
+	remotePeer.SetHeight(pong.Height)
+}

p2pserver/protocols/msg_handler.go
@@ -34,13 +34,15 @@ import (
 	"github.com/DNAProject/DNA/core/types"
 	actor "github.com/DNAProject/DNA/p2pserver/actor/req"
 	msgCommon "github.com/DNAProject/DNA/p2pserver/common"
-	msgpack "github.com/DNAProject/DNA/p2pserver/message/msg_pack"
+	"github.com/DNAProject/DNA/p2pserver/message/msg_pack"
 	msgTypes "github.com/DNAProject/DNA/p2pserver/message/types"
-	p2p "github.com/DNAProject/DNA/p2pserver/net/protocol"
+	"github.com/DNAProject/DNA/p2pserver/net/protocol"
 	"github.com/DNAProject/DNA/p2pserver/protocols/block_sync"
 	"github.com/DNAProject/DNA/p2pserver/protocols/discovery"
+	"github.com/DNAProject/DNA/p2pserver/protocols/heatbeat"
+	"github.com/DNAProject/DNA/p2pserver/protocols/recent_peers"
 	"github.com/DNAProject/DNA/p2pserver/protocols/reconnect"
-	lru "github.com/hashicorp/golang-lru"
+	"github.com/hashicorp/golang-lru"
 )
 
 //respCache cache for some response data
@@ -51,10 +53,12 @@ var respCache *lru.ARCCache
 var txCache, _ = lru.NewARC(msgCommon.MAX_TX_CACHE_SIZE)
 
 type MsgHandler struct {
-	blockSync *block_sync.BlockSyncMgr
-	reconnect *reconnect.ReconnectService
-	discovery *discovery.Discovery
-	ledger    *ledger.Ledger
+	blockSync                *block_sync.BlockSyncMgr
+	reconnect                *reconnect.ReconnectService
+	discovery                *discovery.Discovery
+	heatBeat                 *heatbeat.HeartBeat
+	persistRecentPeerService *recent_peers.PersistRecentPeerService
+	ledger                   *ledger.Ledger
 }
 
 func NewMsgHandler(ld *ledger.Ledger) *MsgHandler {
@@ -65,15 +69,21 @@ func (self *MsgHandler) start(net p2p.P2P) {
 	self.blockSync = block_sync.NewBlockSyncMgr(net, self.ledger)
 	self.reconnect = reconnect.NewReconectService(net)
 	self.discovery = discovery.NewDiscovery(net)
-
+	self.heatBeat = heatbeat.NewHeartBeat(net, self.ledger)
+	self.persistRecentPeerService = recent_peers.NewPersistRecentPeerService(net)
+	go self.persistRecentPeerService.Start()
 	go self.blockSync.Start()
 	go self.reconnect.Start()
 	go self.discovery.Start()
+	go self.heatBeat.Start()
 }
 
 func (self *MsgHandler) stop() {
 	self.blockSync.Close()
 	self.reconnect.Close()
+	self.discovery.Stop()
+	self.persistRecentPeerService.Stop()
+	self.heatBeat.Stop()
 }
 
 func (self *MsgHandler) HandleSystemMessage(net p2p.P2P, msg p2p.SystemMessage) {
@@ -84,10 +94,12 @@ func (self *MsgHandler) HandleSystemMessage(net p2p.P2P, msg p2p.SystemMessage) 
 		self.blockSync.OnAddNode(m.Info.Id)
 		self.reconnect.OnAddPeer(m.Info)
 		self.discovery.OnAddPeer(m.Info)
+		self.persistRecentPeerService.AddNodeAddr(m.Info.Addr + strconv.Itoa(int(m.Info.Port)))
 	case p2p.PeerDisConnected:
 		self.blockSync.OnDelNode(m.Info.Id)
 		self.reconnect.OnDelPeer(m.Info)
 		self.discovery.OnDelPeer(m.Info)
+		self.persistRecentPeerService.DelNodeAddr(m.Info.Addr + strconv.Itoa(int(m.Info.Port)))
 	case p2p.NetworkStop:
 		self.stop()
 	}
@@ -105,9 +117,9 @@ func (self *MsgHandler) HandlePeerMessage(ctx *p2p.Context, msg msgTypes.Message
 	case *msgTypes.HeadersReq:
 		HeadersReqHandle(ctx, m)
 	case *msgTypes.Ping:
-		PingHandle(ctx, m)
+		self.heatBeat.PingHandle(ctx, m)
 	case *msgTypes.Pong:
-		PongHandle(ctx, m)
+		self.heatBeat.PongHandle(ctx, m)
 	case *msgTypes.BlkHeader:
 		self.blockSync.OnHeaderReceive(ctx.Sender().GetID(), m.BlkHdr)
 	case *msgTypes.Block:
@@ -198,28 +210,6 @@ func HeadersReqHandle(ctx *p2p.Context, headersReq *msgTypes.HeadersReq) {
 		log.Warn(err)
 		return
 	}
-}
-
-//PingHandle handle ping msg from peer
-func PingHandle(ctx *p2p.Context, ping *msgTypes.Ping) {
-	remotePeer := ctx.Sender()
-	remotePeer.SetHeight(ping.Height)
-	p2p := ctx.Network()
-
-	height := ledger.DefLedger.GetCurrentBlockHeight()
-	p2p.SetHeight(uint64(height))
-	msg := msgpack.NewPongMsg(uint64(height))
-
-	err := remotePeer.Send(msg)
-	if err != nil {
-		log.Warn(err)
-	}
-}
-
-///PongHandle handle pong msg from peer
-func PongHandle(ctx *p2p.Context, pong *msgTypes.Pong) {
-	remotePeer := ctx.Network()
-	remotePeer.SetHeight(pong.Height)
 }
 
 // blockHandle handles the block message from peer