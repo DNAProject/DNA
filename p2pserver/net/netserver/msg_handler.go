rongyi/ontology

use rand peer kid when DHT refresh

p2pserver/net/netserver/msg_handler.go
@@ -24,7 +24,7 @@ import (
 	"net"
 	"strconv"
 
-	"github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/config"
 	"github.com/ontio/ontology/common/log"
@@ -33,7 +33,7 @@ import (
 	actor "github.com/ontio/ontology/p2pserver/actor/req"
 	msgCommon "github.com/ontio/ontology/p2pserver/common"
 	"github.com/ontio/ontology/p2pserver/dht"
-	"github.com/ontio/ontology/p2pserver/message/msg_pack"
+	msgpack "github.com/ontio/ontology/p2pserver/message/msg_pack"
 	msgTypes "github.com/ontio/ontology/p2pserver/message/types"
 	"github.com/ontio/ontology/p2pserver/protocols"
 )
@@ -48,6 +48,7 @@ var txCache, _ = lru.NewARC(msgCommon.MAX_TX_CACHE_SIZE)
 type MsgHandler struct{}
 
 func (self *MsgHandler) HandleSystemMessage(ctx *protocols.Context, msg protocols.SystemMessage) {
+
 }
 
 func (self *MsgHandler) HandlePeerMessage(ctx *protocols.Context, msg msgTypes.Message) {
@@ -163,10 +164,6 @@ func FindNodeResponseHandle(ctx *protocols.Context, fresp *msgTypes.FindNodeResp
 func FindNodeHandle(ctx *protocols.Context, freq *msgTypes.FindNodeReq) {
 	// we recv message must from establised peer
 	remotePeer := ctx.Sender()
-	if remotePeer == nil {
-		log.Debug("[p2p dht]remotePeer invalid in FindNodeHandle")
-		return
-	}
 
 	var fresp msgTypes.FindNodeResp
 	// check the target is my self

p2pserver/net/netserver/msg_handler_test.go
@@ -35,9 +35,9 @@ import (
 	"github.com/ontio/ontology/events"
 	msgCommon "github.com/ontio/ontology/p2pserver/common"
 	"github.com/ontio/ontology/p2pserver/dht/kbucket"
-	"github.com/ontio/ontology/p2pserver/message/msg_pack"
+	msgpack "github.com/ontio/ontology/p2pserver/message/msg_pack"
 	"github.com/ontio/ontology/p2pserver/message/types"
-	"github.com/ontio/ontology/p2pserver/net/protocol"
+	p2p "github.com/ontio/ontology/p2pserver/net/protocol"
 	"github.com/ontio/ontology/p2pserver/peer"
 	"github.com/ontio/ontology/p2pserver/protocols"
 	"github.com/stretchr/testify/assert"

p2pserver/net/netserver/netserver.go
@@ -26,6 +26,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/ontio/ontology/p2pserver/protocols"
+
 	"github.com/ontio/ontology-eventbus/actor"
 	"github.com/ontio/ontology/common/config"
 	"github.com/ontio/ontology/common/log"
@@ -33,18 +35,19 @@ import (
 	"github.com/ontio/ontology/p2pserver/connect_controller"
 	"github.com/ontio/ontology/p2pserver/dht"
 	"github.com/ontio/ontology/p2pserver/dht/kbucket"
-	"github.com/ontio/ontology/p2pserver/message/msg_pack"
+	msgpack "github.com/ontio/ontology/p2pserver/message/msg_pack"
 	"github.com/ontio/ontology/p2pserver/message/types"
-	"github.com/ontio/ontology/p2pserver/net/protocol"
+	p2p "github.com/ontio/ontology/p2pserver/net/protocol"
 	"github.com/ontio/ontology/p2pserver/peer"
 )
 
 //NewNetServer return the net object in p2p
 func NewNetServer() p2p.P2P {
 	n := &NetServer{
-		NetChan: make(chan *types.MsgPayload, common.CHAN_CAPABILITY),
-		base:    &peer.PeerInfo{},
-		Np:      peer.NewNbrPeers(),
+		NetChan:  make(chan *types.MsgPayload, common.CHAN_CAPABILITY),
+		base:     &peer.PeerInfo{},
+		Np:       peer.NewNbrPeers(),
+		protocol: &MsgHandler{},
 	}
 
 	n.msgRouter = NewMsgRouter(n)
@@ -58,6 +61,7 @@ func NewNetServer() p2p.P2P {
 type NetServer struct {
 	base      *peer.PeerInfo
 	listener  net.Listener
+	protocol  protocols.Protocol
 	msgRouter *MessageRouter
 	pid       *actor.PID
 	NetChan   chan *types.MsgPayload
@@ -503,7 +507,7 @@ func (ns *NetServer) refreshCPL() {
 					if pid.IsPseudoKadId() {
 						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewAddrReq())
 					} else {
-						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewFindNodeReq(pid))
+						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewFindNodeReq(randPeer))
 					}
 				}
 			}

DNAProject/DNA

p2pserver/net/netserver/msg_handler.go
@@ -24,6 +24,9 @@ package netserver
 import (
 	"errors"
 	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/DNAProject/DNA/common"
 	"github.com/DNAProject/DNA/common/config"
 	"github.com/DNAProject/DNA/common/log"
@@ -32,12 +35,10 @@ import (
 	actor "github.com/DNAProject/DNA/p2pserver/actor/req"
 	msgCommon "github.com/DNAProject/DNA/p2pserver/common"
 	"github.com/DNAProject/DNA/p2pserver/dht"
-	"github.com/DNAProject/DNA/p2pserver/message/msg_pack"
+	msgpack "github.com/DNAProject/DNA/p2pserver/message/msg_pack"
 	msgTypes "github.com/DNAProject/DNA/p2pserver/message/types"
 	"github.com/DNAProject/DNA/p2pserver/protocols"
-	"github.com/hashicorp/golang-lru"
-	"net"
-	"strconv"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 //respCache cache for some response data
@@ -50,6 +51,7 @@ var txCache, _ = lru.NewARC(msgCommon.MAX_TX_CACHE_SIZE)
 type MsgHandler struct{}
 
 func (self *MsgHandler) HandleSystemMessage(ctx *protocols.Context, msg protocols.SystemMessage) {
+
 }
 
 func (self *MsgHandler) HandlePeerMessage(ctx *protocols.Context, msg msgTypes.Message) {
@@ -165,10 +167,6 @@ func FindNodeResponseHandle(ctx *protocols.Context, fresp *msgTypes.FindNodeResp
 func FindNodeHandle(ctx *protocols.Context, freq *msgTypes.FindNodeReq) {
 	// we recv message must from establised peer
 	remotePeer := ctx.Sender()
-	if remotePeer == nil {
-		log.Debug("[p2p dht]remotePeer invalid in FindNodeHandle")
-		return
-	}
 
 	var fresp msgTypes.FindNodeResp
 	// check the target is my self

p2pserver/net/netserver/msg_handler_test.go
@@ -41,7 +41,7 @@ import (
 	"github.com/DNAProject/DNA/events"
 	msgCommon "github.com/DNAProject/DNA/p2pserver/common"
 	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
-	"github.com/DNAProject/DNA/p2pserver/message/msg_pack"
+	msgpack "github.com/DNAProject/DNA/p2pserver/message/msg_pack"
 	"github.com/DNAProject/DNA/p2pserver/message/types"
 	p2p "github.com/DNAProject/DNA/p2pserver/net/protocol"
 	"github.com/DNAProject/DNA/p2pserver/peer"

p2pserver/net/netserver/netserver.go
@@ -29,15 +29,17 @@ import (
 	"sync"
 	"time"
 
+	"github.com/DNAProject/DNA/p2pserver/protocols"
+
 	"github.com/DNAProject/DNA/common/config"
 	"github.com/DNAProject/DNA/common/log"
 	"github.com/DNAProject/DNA/p2pserver/common"
 	"github.com/DNAProject/DNA/p2pserver/connect_controller"
 	"github.com/DNAProject/DNA/p2pserver/dht"
 	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
-	"github.com/DNAProject/DNA/p2pserver/message/msg_pack"
+	msgpack "github.com/DNAProject/DNA/p2pserver/message/msg_pack"
 	"github.com/DNAProject/DNA/p2pserver/message/types"
-	"github.com/DNAProject/DNA/p2pserver/net/protocol"
+	p2p "github.com/DNAProject/DNA/p2pserver/net/protocol"
 	"github.com/DNAProject/DNA/p2pserver/peer"
 	"github.com/ontio/ontology-eventbus/actor"
 )
@@ -45,9 +47,10 @@ import (
 //NewNetServer return the net object in p2p
 func NewNetServer() p2p.P2P {
 	n := &NetServer{
-		NetChan: make(chan *types.MsgPayload, common.CHAN_CAPABILITY),
-		base:    &peer.PeerInfo{},
-		Np:      peer.NewNbrPeers(),
+		NetChan:  make(chan *types.MsgPayload, common.CHAN_CAPABILITY),
+		base:     &peer.PeerInfo{},
+		Np:       peer.NewNbrPeers(),
+		protocol: &MsgHandler{},
 	}
 
 	n.msgRouter = NewMsgRouter(n)
@@ -61,6 +64,7 @@ func NewNetServer() p2p.P2P {
 type NetServer struct {
 	base      *peer.PeerInfo
 	listener  net.Listener
+	protocol  protocols.Protocol
 	msgRouter *MessageRouter
 	pid       *actor.PID
 	NetChan   chan *types.MsgPayload
@@ -506,7 +510,7 @@ func (ns *NetServer) refreshCPL() {
 					if pid.IsPseudoKadId() {
 						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewAddrReq())
 					} else {
-						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewFindNodeReq(pid))
+						ns.Send(ns.GetPeer(pid.ToUint64()), msgpack.NewFindNodeReq(randPeer))
 					}
 				}
 			}