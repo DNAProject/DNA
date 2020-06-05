rongyi/ontology

modularize handshake and connect control
* refactor

* exact out handshake logic

* refactor dialer and listener

* add connect controller

* add max inbound limit test

* add handshake with self test case

p2pserver/connect_controller/transport.go
@@ -1,0 +1,115 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package connect_controller
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io/ioutil"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/ontio/ontology/common/config"
+	"github.com/ontio/ontology/p2pserver/common"
+)
+
+type Dialer interface {
+	Dial(nodeAddr string) (net.Conn, error)
+}
+
+func NewDialer(config *config.P2PNodeConfig) (Dialer, error) {
+	if config.IsTLS {
+		return newTlsDialer(config)
+	}
+
+	return &noTlsDialer{}, nil
+}
+
+type tlsDialer struct {
+	config *tls.Config
+}
+
+func newTlsDialer(config *config.P2PNodeConfig) (*tlsDialer, error) {
+	clientCertPool := x509.NewCertPool()
+	cacert, err := ioutil.ReadFile(config.CAPath)
+	if err != nil {
+		return nil, err
+	}
+	cert, err := tls.LoadX509KeyPair(config.CertPath, config.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := clientCertPool.AppendCertsFromPEM(cacert)
+	if !ret {
+		return nil, errors.New("[p2p]failed to parse root certificate")
+	}
+
+	conf := &tls.Config{
+		RootCAs:      clientCertPool,
+		Certificates: []tls.Certificate{cert},
+	}
+
+	return &tlsDialer{config: conf}, nil
+}
+
+func (self *tlsDialer) Dial(nodeAddr string) (net.Conn, error) {
+	var dialer net.Dialer
+	dialer.Timeout = time.Second * common.DIAL_TIMEOUT
+	return tls.DialWithDialer(&dialer, "tcp", nodeAddr, self.config)
+}
+
+type noTlsDialer struct{}
+
+func (self *noTlsDialer) Dial(nodeAddr string) (net.Conn, error) {
+	return net.DialTimeout("tcp", nodeAddr, time.Second*common.DIAL_TIMEOUT)
+}
+
+func NewListener(port uint16, config *config.P2PNodeConfig) (net.Listener, error) {
+	if !config.IsTLS {
+		return net.Listen("tcp", ":"+strconv.Itoa(int(port)))
+	}
+
+	// load cert
+	cert, err := tls.LoadX509KeyPair(config.CertPath, config.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+	// load root ca
+	caData, err := ioutil.ReadFile(config.CAPath)
+	if err != nil {
+		return nil, err
+	}
+	pool := x509.NewCertPool()
+	ret := pool.AppendCertsFromPEM(caData)
+	if !ret {
+		return nil, errors.New("[p2p]failed to parse root certificate")
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      pool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    pool,
+	}
+
+	return tls.Listen("tcp", ":"+strconv.Itoa(int(port)), tlsConfig)
+}

p2pserver/handshake/handshake.go
@@ -1,0 +1,186 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package handshake
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	common2 "github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/p2pserver/common"
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
+	"github.com/ontio/ontology/p2pserver/message/types"
+	"github.com/ontio/ontology/p2pserver/peer"
+)
+
+const HANDSHAKE_DURATION = 10 * time.Second // handshake time can not exceed this duration, or will treat as attack.
+
+func HandshakeClient(info *peer.PeerInfo, selfId *kbucket.KadKeyId, conn net.Conn) (*peer.PeerInfo, error) {
+	version := newVersion(info)
+	if err := conn.SetDeadline(time.Now().Add(HANDSHAKE_DURATION)); err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = conn.SetDeadline(time.Time{}) //reset back
+	}()
+
+	// 1. sendMsg version
+	err := sendMsg(conn, version)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. read version
+	msg, _, err := types.ReadMessage(conn)
+	if err != nil {
+		return nil, err
+	}
+	receivedVersion, ok := msg.(*types.Version)
+	if !ok {
+		return nil, fmt.Errorf("expected version message, but got message type: %s", msg.CmdType())
+	}
+
+	// 3. update kadId
+	kid := kbucket.PseudoKadIdFromUint64(receivedVersion.P.Nonce)
+	if receivedVersion.P.SoftVersion > "v1.9.0" {
+		err = sendMsg(conn, &types.UpdateKadId{KadKeyId: selfId})
+		if err != nil {
+			return nil, err
+		}
+		// 4. read kadkeyid
+		msg, _, err = types.ReadMessage(conn)
+		if err != nil {
+			return nil, err
+		}
+		kadKeyId, ok := msg.(*types.UpdateKadId)
+		if !ok {
+			return nil, fmt.Errorf("handshake failed, expect kad id message, got %s", msg.CmdType())
+		}
+
+		kid = kadKeyId.KadKeyId.Id
+	}
+
+	// 5. sendMsg ack
+	err = sendMsg(conn, &types.VerACK{})
+	if err != nil {
+		return nil, err
+	}
+
+	return createPeerInfo(receivedVersion, kid), nil
+}
+
+func HandshakeServer(info *peer.PeerInfo, selfId *kbucket.KadKeyId, conn net.Conn) (*peer.PeerInfo, error) {
+	ver := newVersion(info)
+	if err := conn.SetDeadline(time.Now().Add(HANDSHAKE_DURATION)); err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = conn.SetDeadline(time.Time{}) //reset back
+	}()
+
+	// 1. read version
+	msg, _, err := types.ReadMessage(conn)
+	if err != nil {
+		return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+	}
+	if msg.CmdType() != common.VERSION_TYPE {
+		return nil, fmt.Errorf("[HandshakeServer] expected version message")
+	}
+	version := msg.(*types.Version)
+
+	// 2. sendMsg version
+	err = sendMsg(conn, ver)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. read update kadkey id
+	kid := kbucket.PseudoKadIdFromUint64(version.P.Nonce)
+	if version.P.SoftVersion > "v1.9.0" {
+		msg, _, err := types.ReadMessage(conn)
+		if err != nil {
+			return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+		}
+		kadkeyId, ok := msg.(*types.UpdateKadId)
+		if !ok {
+			return nil, fmt.Errorf("[HandshakeServer] expected update kadkeyid message")
+		}
+		kid = kadkeyId.KadKeyId.Id
+		// 4. sendMsg update kadkey id
+		err = sendMsg(conn, &types.UpdateKadId{KadKeyId: selfId})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// 5. read version ack
+	msg, _, err = types.ReadMessage(conn)
+	if err != nil {
+		return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+	}
+	if msg.CmdType() != common.VERACK_TYPE {
+		return nil, fmt.Errorf("[HandshakeServer] expected version ack message")
+	}
+
+	return createPeerInfo(version, kid), nil
+}
+
+func sendMsg(conn net.Conn, msg types.Message) error {
+	sink := common2.NewZeroCopySink(nil)
+	types.WriteMessage(sink, msg)
+	_, err := conn.Write(sink.Bytes())
+	if err != nil {
+		return fmt.Errorf("[handshake]error sending messge to %s :%s", conn.LocalAddr(), err.Error())
+	}
+
+	return nil
+}
+
+func createPeerInfo(version *types.Version, kid kbucket.KadId) *peer.PeerInfo {
+	return peer.NewPeerInfo(kid, version.P.Version, version.P.Services, version.P.Relay != 0, version.P.HttpInfoPort,
+		version.P.SyncPort, version.P.StartHeight, version.P.SoftVersion)
+}
+
+func newVersion(peerInfo *peer.PeerInfo) *types.Version {
+	var version types.Version
+	version.P = types.VersionPayload{
+		Version:      peerInfo.Version,
+		Services:     peerInfo.Services,
+		SyncPort:     peerInfo.Port,
+		Nonce:        peerInfo.Id.ToUint64(),
+		IsConsensus:  false,
+		HttpInfoPort: peerInfo.HttpInfoPort,
+		StartHeight:  peerInfo.Height,
+		TimeStamp:    time.Now().UnixNano(),
+		SoftVersion:  peerInfo.SoftVersion,
+	}
+
+	if peerInfo.Relay {
+		version.P.Relay = 1
+	} else {
+		version.P.Relay = 0
+	}
+	if peerInfo.HttpInfoPort > 0 {
+		version.P.Cap[common.HTTP_INFO_FLAG] = 0x01
+	} else {
+		version.P.Cap[common.HTTP_INFO_FLAG] = 0x00
+	}
+
+	return &version
+}

p2pserver/handshake/handshake_test.go
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package handshake
+
+import (
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
+	"github.com/ontio/ontology/p2pserver/message/types"
+	"github.com/ontio/ontology/p2pserver/peer"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	kbucket.Difficulty = 1
+}
+
+type Node struct {
+	Id   *kbucket.KadKeyId
+	Info *peer.PeerInfo
+	Conn net.Conn
+}
+
+func NewNode(conn net.Conn) Node {
+	node := Node{
+		Id:   kbucket.RandKadKeyId(),
+		Info: &peer.PeerInfo{},
+		Conn: conn,
+	}
+	node.Info.Id = node.Id.Id
+	node.Info.SoftVersion = "v1.9.0-beta"
+
+	return node
+}
+
+func NewPair() (client Node, server Node) {
+	c, s := net.Pipe()
+
+	client = NewNode(c)
+	server = NewNode(s)
+	return
+}
+
+func TestHandshakeNormal(t *testing.T) {
+	client, server := NewPair()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		peer, err := HandshakeClient(client.Info, client.Id, client.Conn)
+		assert.Nil(t, err)
+		assert.Equal(t, peer.Id, server.Id.Id)
+		wg.Done()
+	}()
+	go func() {
+		peer, err := HandshakeServer(server.Info, server.Id, server.Conn)
+		assert.Nil(t, err)
+		assert.Equal(t, peer.Id, client.Id.Id)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func TestHandshakeTimeout(t *testing.T) {
+	client, _ := NewPair()
+
+	_, err := HandshakeClient(client.Info, client.Id, client.Conn)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "deadline exceeded")
+}
+
+func TestHandshakeWrongMsg(t *testing.T) {
+	client, server := NewPair()
+	go func() {
+		err := sendMsg(client.Conn, &types.Addr{})
+		assert.Nil(t, err)
+	}()
+
+	_, err := HandshakeServer(server.Info, server.Id, server.Conn)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "expected version message")
+}

p2pserver/peer/nbr_peer_test.go
@@ -20,6 +20,7 @@ package peer
 
 import (
 	"fmt"
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
 	"testing"
 	"time"
 )
@@ -27,11 +28,10 @@ import (
 func createPeers(cnt uint16) []*Peer {
 	np := []*Peer{}
 	var syncport uint16
-	var id uint64
 	var height uint64
 	for i := uint16(0); i < cnt; i++ {
 		syncport = 20224 + i
-		id = 0x7533345 + uint64(i)
+		id := kbucket.PseudoKadIdFromUint64(0x7533345 + uint64(i))
 		height = 434923 + uint64(i)
 		p := NewPeer()
 		p.UpdateInfo(time.Now(), 2, 3, syncport, id, 0, height, "1.5.2")
@@ -45,8 +45,7 @@ func createPeers(cnt uint16) []*Peer {
 }
 
 func initTestNbrPeers() *NbrPeers {
-	nm := &NbrPeers{}
-	nm.Init()
+	nm := NewNbrPeers()
 	np := createPeers(5)
 	for _, v := range np {
 		nm.List[v.GetID()] = v
@@ -71,23 +70,6 @@ func TestGetPeer(t *testing.T) {
 	p := nm.GetPeer(0x7533345)
 	if p == nil {
 		t.Fatal("TestGetPeer error")
-	}
-}
-
-func TestAddNbrNode(t *testing.T) {
-	nm := initTestNbrPeers()
-
-	p := NewPeer()
-	p.UpdateInfo(time.Now(), 2, 3, 10335, 0x7123456, 0, 100, "1.5.2")
-	p.SetState(3)
-	p.SetHttpInfoState(true)
-	p.Link.SetAddr("127.0.0.1")
-	nm.AddNbrNode(p)
-	if !nm.NodeExisted(0x7123456) {
-		t.Fatal("0x7123456 should be added in nbr peer")
-	}
-	if len(nm.List) != 6 {
-		t.Fatal("0x7123456 should be added in nbr peer")
 	}
 }
 

DNAProject/DNA

p2pserver/connect_controller/transport.go
@@ -1,0 +1,118 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package connect_controller
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io/ioutil"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/DNAProject/DNA/common/config"
+	"github.com/DNAProject/DNA/p2pserver/common"
+)
+
+type Dialer interface {
+	Dial(nodeAddr string) (net.Conn, error)
+}
+
+func NewDialer(config *config.P2PNodeConfig) (Dialer, error) {
+	if config.IsTLS {
+		return newTlsDialer(config)
+	}
+
+	return &noTlsDialer{}, nil
+}
+
+type tlsDialer struct {
+	config *tls.Config
+}
+
+func newTlsDialer(config *config.P2PNodeConfig) (*tlsDialer, error) {
+	clientCertPool := x509.NewCertPool()
+	cacert, err := ioutil.ReadFile(config.CAPath)
+	if err != nil {
+		return nil, err
+	}
+	cert, err := tls.LoadX509KeyPair(config.CertPath, config.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := clientCertPool.AppendCertsFromPEM(cacert)
+	if !ret {
+		return nil, errors.New("[p2p]failed to parse root certificate")
+	}
+
+	conf := &tls.Config{
+		RootCAs:      clientCertPool,
+		Certificates: []tls.Certificate{cert},
+	}
+
+	return &tlsDialer{config: conf}, nil
+}
+
+func (self *tlsDialer) Dial(nodeAddr string) (net.Conn, error) {
+	var dialer net.Dialer
+	dialer.Timeout = time.Second * common.DIAL_TIMEOUT
+	return tls.DialWithDialer(&dialer, "tcp", nodeAddr, self.config)
+}
+
+type noTlsDialer struct{}
+
+func (self *noTlsDialer) Dial(nodeAddr string) (net.Conn, error) {
+	return net.DialTimeout("tcp", nodeAddr, time.Second*common.DIAL_TIMEOUT)
+}
+
+func NewListener(port uint16, config *config.P2PNodeConfig) (net.Listener, error) {
+	if !config.IsTLS {
+		return net.Listen("tcp", ":"+strconv.Itoa(int(port)))
+	}
+
+	// load cert
+	cert, err := tls.LoadX509KeyPair(config.CertPath, config.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+	// load root ca
+	caData, err := ioutil.ReadFile(config.CAPath)
+	if err != nil {
+		return nil, err
+	}
+	pool := x509.NewCertPool()
+	ret := pool.AppendCertsFromPEM(caData)
+	if !ret {
+		return nil, errors.New("[p2p]failed to parse root certificate")
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      pool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    pool,
+	}
+
+	return tls.Listen("tcp", ":"+strconv.Itoa(int(port)), tlsConfig)
+}

p2pserver/handshake/handshake.go
@@ -1,0 +1,189 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package handshake
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	common2 "github.com/DNAProject/DNA/common"
+	"github.com/DNAProject/DNA/p2pserver/common"
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
+	"github.com/DNAProject/DNA/p2pserver/message/types"
+	"github.com/DNAProject/DNA/p2pserver/peer"
+)
+
+const HANDSHAKE_DURATION = 10 * time.Second // handshake time can not exceed this duration, or will treat as attack.
+
+func HandshakeClient(info *peer.PeerInfo, selfId *kbucket.KadKeyId, conn net.Conn) (*peer.PeerInfo, error) {
+	version := newVersion(info)
+	if err := conn.SetDeadline(time.Now().Add(HANDSHAKE_DURATION)); err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = conn.SetDeadline(time.Time{}) //reset back
+	}()
+
+	// 1. sendMsg version
+	err := sendMsg(conn, version)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. read version
+	msg, _, err := types.ReadMessage(conn)
+	if err != nil {
+		return nil, err
+	}
+	receivedVersion, ok := msg.(*types.Version)
+	if !ok {
+		return nil, fmt.Errorf("expected version message, but got message type: %s", msg.CmdType())
+	}
+
+	// 3. update kadId
+	kid := kbucket.PseudoKadIdFromUint64(receivedVersion.P.Nonce)
+	if receivedVersion.P.SoftVersion > "v1.9.0" {
+		err = sendMsg(conn, &types.UpdateKadId{KadKeyId: selfId})
+		if err != nil {
+			return nil, err
+		}
+		// 4. read kadkeyid
+		msg, _, err = types.ReadMessage(conn)
+		if err != nil {
+			return nil, err
+		}
+		kadKeyId, ok := msg.(*types.UpdateKadId)
+		if !ok {
+			return nil, fmt.Errorf("handshake failed, expect kad id message, got %s", msg.CmdType())
+		}
+
+		kid = kadKeyId.KadKeyId.Id
+	}
+
+	// 5. sendMsg ack
+	err = sendMsg(conn, &types.VerACK{})
+	if err != nil {
+		return nil, err
+	}
+
+	return createPeerInfo(receivedVersion, kid), nil
+}
+
+func HandshakeServer(info *peer.PeerInfo, selfId *kbucket.KadKeyId, conn net.Conn) (*peer.PeerInfo, error) {
+	ver := newVersion(info)
+	if err := conn.SetDeadline(time.Now().Add(HANDSHAKE_DURATION)); err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = conn.SetDeadline(time.Time{}) //reset back
+	}()
+
+	// 1. read version
+	msg, _, err := types.ReadMessage(conn)
+	if err != nil {
+		return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+	}
+	if msg.CmdType() != common.VERSION_TYPE {
+		return nil, fmt.Errorf("[HandshakeServer] expected version message")
+	}
+	version := msg.(*types.Version)
+
+	// 2. sendMsg version
+	err = sendMsg(conn, ver)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. read update kadkey id
+	kid := kbucket.PseudoKadIdFromUint64(version.P.Nonce)
+	if version.P.SoftVersion > "v1.9.0" {
+		msg, _, err := types.ReadMessage(conn)
+		if err != nil {
+			return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+		}
+		kadkeyId, ok := msg.(*types.UpdateKadId)
+		if !ok {
+			return nil, fmt.Errorf("[HandshakeServer] expected update kadkeyid message")
+		}
+		kid = kadkeyId.KadKeyId.Id
+		// 4. sendMsg update kadkey id
+		err = sendMsg(conn, &types.UpdateKadId{KadKeyId: selfId})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// 5. read version ack
+	msg, _, err = types.ReadMessage(conn)
+	if err != nil {
+		return nil, fmt.Errorf("[HandshakeServer] ReadMessage failed, error: %s", err)
+	}
+	if msg.CmdType() != common.VERACK_TYPE {
+		return nil, fmt.Errorf("[HandshakeServer] expected version ack message")
+	}
+
+	return createPeerInfo(version, kid), nil
+}
+
+func sendMsg(conn net.Conn, msg types.Message) error {
+	sink := common2.NewZeroCopySink(nil)
+	types.WriteMessage(sink, msg)
+	_, err := conn.Write(sink.Bytes())
+	if err != nil {
+		return fmt.Errorf("[handshake]error sending messge to %s :%s", conn.LocalAddr(), err.Error())
+	}
+
+	return nil
+}
+
+func createPeerInfo(version *types.Version, kid kbucket.KadId) *peer.PeerInfo {
+	return peer.NewPeerInfo(kid, version.P.Version, version.P.Services, version.P.Relay != 0, version.P.HttpInfoPort,
+		version.P.SyncPort, version.P.StartHeight, version.P.SoftVersion)
+}
+
+func newVersion(peerInfo *peer.PeerInfo) *types.Version {
+	var version types.Version
+	version.P = types.VersionPayload{
+		Version:      peerInfo.Version,
+		Services:     peerInfo.Services,
+		SyncPort:     peerInfo.Port,
+		Nonce:        peerInfo.Id.ToUint64(),
+		IsConsensus:  false,
+		HttpInfoPort: peerInfo.HttpInfoPort,
+		StartHeight:  peerInfo.Height,
+		TimeStamp:    time.Now().UnixNano(),
+		SoftVersion:  peerInfo.SoftVersion,
+	}
+
+	if peerInfo.Relay {
+		version.P.Relay = 1
+	} else {
+		version.P.Relay = 0
+	}
+	if peerInfo.HttpInfoPort > 0 {
+		version.P.Cap[common.HTTP_INFO_FLAG] = 0x01
+	} else {
+		version.P.Cap[common.HTTP_INFO_FLAG] = 0x00
+	}
+
+	return &version
+}

p2pserver/handshake/handshake_test.go
@@ -1,0 +1,103 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package handshake
+
+import (
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
+	"github.com/DNAProject/DNA/p2pserver/message/types"
+	"github.com/DNAProject/DNA/p2pserver/peer"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	kbucket.Difficulty = 1
+}
+
+type Node struct {
+	Id   *kbucket.KadKeyId
+	Info *peer.PeerInfo
+	Conn net.Conn
+}
+
+func NewNode(conn net.Conn) Node {
+	node := Node{
+		Id:   kbucket.RandKadKeyId(),
+		Info: &peer.PeerInfo{},
+		Conn: conn,
+	}
+	node.Info.Id = node.Id.Id
+	node.Info.SoftVersion = "v1.9.0-beta"
+
+	return node
+}
+
+func NewPair() (client Node, server Node) {
+	c, s := net.Pipe()
+
+	client = NewNode(c)
+	server = NewNode(s)
+	return
+}
+
+func TestHandshakeNormal(t *testing.T) {
+	client, server := NewPair()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		peer, err := HandshakeClient(client.Info, client.Id, client.Conn)
+		assert.Nil(t, err)
+		assert.Equal(t, peer.Id, server.Id.Id)
+		wg.Done()
+	}()
+	go func() {
+		peer, err := HandshakeServer(server.Info, server.Id, server.Conn)
+		assert.Nil(t, err)
+		assert.Equal(t, peer.Id, client.Id.Id)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func TestHandshakeTimeout(t *testing.T) {
+	client, _ := NewPair()
+
+	_, err := HandshakeClient(client.Info, client.Id, client.Conn)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "deadline exceeded")
+}
+
+func TestHandshakeWrongMsg(t *testing.T) {
+	client, server := NewPair()
+	go func() {
+		err := sendMsg(client.Conn, &types.Addr{})
+		assert.Nil(t, err)
+	}()
+
+	_, err := HandshakeServer(server.Info, server.Id, server.Conn)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "expected version message")
+}

p2pserver/peer/nbr_peer_test.go
@@ -23,6 +23,7 @@ package peer
 
 import (
 	"fmt"
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
 	"testing"
 	"time"
 )
@@ -30,11 +31,10 @@ import (
 func createPeers(cnt uint16) []*Peer {
 	np := []*Peer{}
 	var syncport uint16
-	var id uint64
 	var height uint64
 	for i := uint16(0); i < cnt; i++ {
 		syncport = 20224 + i
-		id = 0x7533345 + uint64(i)
+		id := kbucket.PseudoKadIdFromUint64(0x7533345 + uint64(i))
 		height = 434923 + uint64(i)
 		p := NewPeer()
 		p.UpdateInfo(time.Now(), 2, 3, syncport, id, 0, height, "1.5.2")
@@ -48,8 +48,7 @@ func createPeers(cnt uint16) []*Peer {
 }
 
 func initTestNbrPeers() *NbrPeers {
-	nm := &NbrPeers{}
-	nm.Init()
+	nm := NewNbrPeers()
 	np := createPeers(5)
 	for _, v := range np {
 		nm.List[v.GetID()] = v
@@ -74,23 +73,6 @@ func TestGetPeer(t *testing.T) {
 	p := nm.GetPeer(0x7533345)
 	if p == nil {
 		t.Fatal("TestGetPeer error")
-	}
-}
-
-func TestAddNbrNode(t *testing.T) {
-	nm := initTestNbrPeers()
-
-	p := NewPeer()
-	p.UpdateInfo(time.Now(), 2, 3, 10335, 0x7123456, 0, 100, "1.5.2")
-	p.SetState(3)
-	p.SetHttpInfoState(true)
-	p.Link.SetAddr("127.0.0.1")
-	nm.AddNbrNode(p)
-	if !nm.NodeExisted(0x7123456) {
-		t.Fatal("0x7123456 should be added in nbr peer")
-	}
-	if len(nm.List) != 6 {
-		t.Fatal("0x7123456 should be added in nbr peer")
 	}
 }
 