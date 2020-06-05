rongyi/ontology

fix handshake with self

p2pserver/peer/nbr_peer_test.go
@@ -20,9 +20,10 @@ package peer
 
 import (
 	"fmt"
-	"github.com/ontio/ontology/p2pserver/dht/kbucket"
 	"testing"
 	"time"
+
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
 )
 
 func createPeers(cnt uint16) []*Peer {

DNAProject/DNA

p2pserver/peer/nbr_peer_test.go
@@ -23,9 +23,10 @@ package peer
 
 import (
 	"fmt"
-	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
 	"testing"
 	"time"
+
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
 )
 
 func createPeers(cnt uint16) []*Peer {