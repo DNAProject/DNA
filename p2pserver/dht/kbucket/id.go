rongyi/ontology

init dht implement
* Add kbucket

* Update kbucket

* kbucket done

* Update dht

* Update dht

* Add file header

* Add log

* Update dht

change some comment

* go fmt

* update dht id

* clean up

* aaa

* go fmt

* clean up

* ishandwithself

* add license

* use kad

* Update p2pserver/net/netserver/handshake.go

* Update p2pserver/net/netserver/handshake.go

* Update p2pserver/dht/dht.go

* Update p2pserver/dht/dht.go

* add license

* fix kadid deserialize bug

* fix kadid serialize

* fix HandshakeServer peer link setid

* Fix test failure (#50)

* Fix test failure

* Lower difficulty in test and add maxCplForRefresh guard check

* Change type in test

* Fix test small error

Co-authored-by: rongyi <rongyi@onchain.com>
Co-authored-by: rongyi <sqrongyi@163.com>
Co-authored-by: lucas7788 <sishsh@163.com>

p2pserver/dht/kbucket/id.go
@@ -1,0 +1,226 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package kbucket
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"errors"
+	"io"
+	"math"
+	"math/big"
+	"math/bits"
+
+	"github.com/ontio/ontology-crypto/keypair"
+	"github.com/ontio/ontology/account"
+	"github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/core/types"
+)
+
+var Difficulty = 18 //bit
+
+type KadId struct {
+	val common.Address
+}
+
+func (self KadId) Serialization(sink *common.ZeroCopySink) {
+	sink.WriteAddress(self.val)
+}
+
+func (self *KadId) Deserialization(source *common.ZeroCopySource) error {
+	val, eof := source.NextAddress()
+	if eof {
+		return io.ErrUnexpectedEOF
+	}
+	self.val = val
+	return nil
+}
+
+func (self *KadId) ToHexString() string {
+	return self.val.ToHexString()
+}
+
+type KadKeyId struct {
+	PublicKey keypair.PublicKey
+
+	Id KadId
+}
+
+func (self KadId) GenRandKadId(prefix uint) KadId {
+	var ret KadId
+	rand.Read(ret.val[:])
+	if prefix == 0 {
+		ret.val[0] &= 0x7f
+		// make first bit different
+		if (0x80 & self.val[0]) == 0 {
+			ret.val[0] |= 0x80
+		}
+		return ret
+	}
+
+	num := prefix / 8
+	left := prefix % 8
+	if num > uint(len(self.val[:])) {
+		num = uint(len(self.val[:]))
+	}
+
+	if num > 0 {
+		copy(ret.val[:num], self.val[:num])
+	}
+	// make all prefix bits same
+	mask := (uint8(0xff) >> (8 - left)) << (8 - left)
+	ret.val[num] &= (^mask)
+	ret.val[num] |= (self.val[num] & mask)
+
+	// and prefix + 1 different
+	// clear prefix + 1
+	ret.val[num] &= ^(1 << (8 - left - 1))
+	// if prefix + 1 bit is 1 then we already satisfied
+	// if prefix + 1 bit is 0, then we should set this bit
+	if (self.val[num]>>(8-left-1))&1 == 0 {
+		ret.val[num] |= (1 << (8 - left - 1))
+	}
+
+	return ret
+}
+
+func (self KadId) ToUint64() uint64 {
+	if self.isPseudoKadId() {
+		nonce := binary.LittleEndian.Uint64(self.val[:8])
+		return nonce
+	}
+	kid := new(big.Int).SetBytes(self.val[:])
+	uint64Max := new(big.Int).SetUint64(math.MaxUint64)
+	res := kid.Mod(kid, uint64Max)
+	return res.Uint64()
+}
+
+func (self KadId) isPseudoKadId() bool {
+	for i := 8; i < len(self.val); i++ {
+		if self.val[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PseudoKadIdFromUint64(data uint64) KadId {
+	id := common.ADDRESS_EMPTY
+	binary.LittleEndian.PutUint64(id[:], data)
+	return KadId{
+		val: id,
+	}
+}
+
+func (this *KadKeyId) Serialization(sink *common.ZeroCopySink) {
+	sink.WriteVarBytes(keypair.SerializePublicKey(this.PublicKey))
+}
+
+func (this *KadKeyId) Deserialization(source *common.ZeroCopySource) error {
+	data, _, irregular, eof := source.NextVarBytes()
+	if irregular {
+		return common.ErrIrregularData
+	}
+	if eof {
+		return io.ErrUnexpectedEOF
+	}
+	pub, err := keypair.DeserializePublicKey(data)
+	if err != nil {
+		return err
+	}
+	if !validatePublicKey(pub) {
+		return errors.New("invalid kad public key")
+	}
+	this.PublicKey = pub
+	this.Id = kadIdFromPubkey(pub)
+	return nil
+}
+
+func kadIdFromPubkey(pubKey keypair.PublicKey) KadId {
+	return KadId{val: types.AddressFromPubKey(pubKey)}
+}
+
+func RandKadKeyId() *KadKeyId {
+	var acc *account.Account
+	for {
+		acc = account.NewAccount("")
+		if validatePublicKey(acc.PublicKey) {
+			break
+		}
+	}
+	kid := kadIdFromPubkey(acc.PublicKey)
+	return &KadKeyId{
+		PublicKey: acc.PublicKey,
+		Id:        kid,
+	}
+}
+
+func validatePublicKey(pubKey keypair.PublicKey) bool {
+	pub := keypair.SerializePublicKey(pubKey)
+	res := sha256.Sum256(pub)
+	hash := sha256.Sum256(res[:])
+	limit := Difficulty >> 3
+	for i := 0; i < limit; i++ {
+		if hash[i] != 0 {
+			return false
+		}
+	}
+	diff := Difficulty % 8
+	if diff != 0 {
+		x := hash[limit] >> uint8(8-diff)
+		return x == 0
+	}
+	return true
+}
+
+func (self KadId) distance(b KadId) KadId {
+	var c KadId
+	for i := 0; i < len(self.val); i++ {
+		c.val[i] = self.val[i] ^ b.val[i]
+	}
+
+	return c
+}
+
+// Closer returns true if a is closer to self than b is
+func (self KadId) Closer(a, b KadId) bool {
+	adist := self.distance(a)
+	bdist := self.distance(b)
+
+	return bytes.Compare(adist.val[:], bdist.val[:]) < 0
+}
+
+// CommonPrefixLen(cpl) calculate two ID's xor prefix 0
+func CommonPrefixLen(a, b KadId) int {
+	dis := a.distance(b)
+	return zeroPrefixLen(dis.val[:])
+}
+
+// ZeroPrefixLen returns the number of consecutive zeroes in a byte slice.
+func zeroPrefixLen(id []byte) int {
+	for i, b := range id {
+		if b != 0 {
+			return i*8 + bits.LeadingZeros8(uint8(b))
+		}
+	}
+
+	return len(id) * 8
+}

p2pserver/message/msg_pack/msg_pack.go
@@ -26,6 +26,7 @@ import (
 	"github.com/ontio/ontology/common/log"
 	ct "github.com/ontio/ontology/core/types"
 	msgCommon "github.com/ontio/ontology/p2pserver/common"
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
 	mt "github.com/ontio/ontology/p2pserver/message/types"
 	p2pnet "github.com/ontio/ontology/p2pserver/net/protocol"
 )
@@ -140,6 +141,14 @@ func NewTxn(txn *ct.Transaction) mt.Message {
 	return &trn
 }
 
+func NewUpdateKadKeyId(n p2pnet.P2P) mt.Message {
+	log.Trace()
+	upk := &mt.UpdateKadId{
+		KadKeyId: n.GetKadKeyId(),
+	}
+	return upk
+}
+
 //version ack package
 func NewVerAck() mt.Message {
 	log.Trace()
@@ -205,4 +214,12 @@ func NewConsensusDataReq(hash common.Uint256) mt.Message {
 	dataReq.Hash = hash
 
 	return &dataReq
+}
+
+func NewFindNodeReq(id kbucket.KadId) mt.Message {
+	req := mt.FindNodeReq{
+		TargetID: id,
+	}
+
+	return &req
 }

p2pserver/message/types/find_node_test.go
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package types
+
+import (
+	"testing"
+
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
+)
+
+func TestFindNodeRequest(t *testing.T) {
+	var req FindNodeReq
+	req.TargetID = kbucket.KadId{}
+
+	MessageTest(t, &req)
+}
+
+func TestFindNodeResponse(t *testing.T) {
+	var resp FindNodeResp
+	resp.TargetID = kbucket.KadId{}
+	resp.Address = "127.0.0.1:1222"
+	resp.CloserPeers = []PeerAddr{
+		PeerAddr{
+			PeerID: 0x456,
+			Addr:   "127.0.0.1:4222",
+		},
+	}
+	resp.Success = true
+
+	MessageTest(t, &resp)
+}

p2pserver/net/protocol/server.go
@@ -20,7 +20,9 @@
 package p2p
 
 import (
+	"github.com/ontio/ontology-eventbus/actor"
 	"github.com/ontio/ontology/p2pserver/common"
+	"github.com/ontio/ontology/p2pserver/dht/kbucket"
 	"github.com/ontio/ontology/p2pserver/message/types"
 	"github.com/ontio/ontology/p2pserver/peer"
 )
@@ -31,6 +33,7 @@ type P2P interface {
 	Halt()
 	Connect(addr string) error
 	GetID() uint64
+	GetKId() kbucket.KadId
 	GetVersion() uint32
 	GetPort() uint16
 	GetHttpInfoPort() uint16
@@ -43,7 +46,7 @@ type P2P interface {
 	GetConnectionCnt() uint32
 	GetMaxPeerBlockHeight() uint64
 	GetNp() *peer.NbrPeers
-	GetPeer(uint64) *peer.Peer
+	GetPeer(id uint64) *peer.Peer
 	SetHeight(uint64)
 	IsPeerEstablished(p *peer.Peer) bool
 	Send(p *peer.Peer, msg types.Message) error
@@ -59,9 +62,17 @@ type P2P interface {
 	RemovePeerAddress(addr string)
 	AddNbrNode(*peer.Peer)
 	DelNbrNode(id uint64) (*peer.Peer, bool)
-	NodeEstablished(uint64) bool
+	NodeEstablished(id uint64) bool
 	Xmit(msg types.Message)
 	SetOwnAddress(addr string)
 	IsOwnAddress(addr string) bool
 	IsAddrFromConnecting(addr string) bool
+
+	UpdateDHT(id kbucket.KadId) bool
+	RemoveDHT(id kbucket.KadId) bool
+	BetterPeers(id kbucket.KadId, count int) []kbucket.KadId
+	GetKadKeyId() *kbucket.KadKeyId
+
+	GetPeerStringAddr() map[uint64]string
+	SetPID(pid *actor.PID)
 }

DNAProject/DNA

p2pserver/dht/kbucket/id.go
@@ -1,0 +1,229 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package kbucket
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"errors"
+	"io"
+	"math"
+	"math/big"
+	"math/bits"
+
+	"github.com/DNAProject/DNA/account"
+	"github.com/DNAProject/DNA/common"
+	"github.com/DNAProject/DNA/core/types"
+	"github.com/ontio/ontology-crypto/keypair"
+)
+
+var Difficulty = 18 //bit
+
+type KadId struct {
+	val common.Address
+}
+
+func (self KadId) Serialization(sink *common.ZeroCopySink) {
+	sink.WriteAddress(self.val)
+}
+
+func (self *KadId) Deserialization(source *common.ZeroCopySource) error {
+	val, eof := source.NextAddress()
+	if eof {
+		return io.ErrUnexpectedEOF
+	}
+	self.val = val
+	return nil
+}
+
+func (self *KadId) ToHexString() string {
+	return self.val.ToHexString()
+}
+
+type KadKeyId struct {
+	PublicKey keypair.PublicKey
+
+	Id KadId
+}
+
+func (self KadId) GenRandKadId(prefix uint) KadId {
+	var ret KadId
+	rand.Read(ret.val[:])
+	if prefix == 0 {
+		ret.val[0] &= 0x7f
+		// make first bit different
+		if (0x80 & self.val[0]) == 0 {
+			ret.val[0] |= 0x80
+		}
+		return ret
+	}
+
+	num := prefix / 8
+	left := prefix % 8
+	if num > uint(len(self.val[:])) {
+		num = uint(len(self.val[:]))
+	}
+
+	if num > 0 {
+		copy(ret.val[:num], self.val[:num])
+	}
+	// make all prefix bits same
+	mask := (uint8(0xff) >> (8 - left)) << (8 - left)
+	ret.val[num] &= (^mask)
+	ret.val[num] |= (self.val[num] & mask)
+
+	// and prefix + 1 different
+	// clear prefix + 1
+	ret.val[num] &= ^(1 << (8 - left - 1))
+	// if prefix + 1 bit is 1 then we already satisfied
+	// if prefix + 1 bit is 0, then we should set this bit
+	if (self.val[num]>>(8-left-1))&1 == 0 {
+		ret.val[num] |= (1 << (8 - left - 1))
+	}
+
+	return ret
+}
+
+func (self KadId) ToUint64() uint64 {
+	if self.isPseudoKadId() {
+		nonce := binary.LittleEndian.Uint64(self.val[:8])
+		return nonce
+	}
+	kid := new(big.Int).SetBytes(self.val[:])
+	uint64Max := new(big.Int).SetUint64(math.MaxUint64)
+	res := kid.Mod(kid, uint64Max)
+	return res.Uint64()
+}
+
+func (self KadId) isPseudoKadId() bool {
+	for i := 8; i < len(self.val); i++ {
+		if self.val[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PseudoKadIdFromUint64(data uint64) KadId {
+	id := common.ADDRESS_EMPTY
+	binary.LittleEndian.PutUint64(id[:], data)
+	return KadId{
+		val: id,
+	}
+}
+
+func (this *KadKeyId) Serialization(sink *common.ZeroCopySink) {
+	sink.WriteVarBytes(keypair.SerializePublicKey(this.PublicKey))
+}
+
+func (this *KadKeyId) Deserialization(source *common.ZeroCopySource) error {
+	data, _, irregular, eof := source.NextVarBytes()
+	if irregular {
+		return common.ErrIrregularData
+	}
+	if eof {
+		return io.ErrUnexpectedEOF
+	}
+	pub, err := keypair.DeserializePublicKey(data)
+	if err != nil {
+		return err
+	}
+	if !validatePublicKey(pub) {
+		return errors.New("invalid kad public key")
+	}
+	this.PublicKey = pub
+	this.Id = kadIdFromPubkey(pub)
+	return nil
+}
+
+func kadIdFromPubkey(pubKey keypair.PublicKey) KadId {
+	return KadId{val: types.AddressFromPubKey(pubKey)}
+}
+
+func RandKadKeyId() *KadKeyId {
+	var acc *account.Account
+	for {
+		acc = account.NewAccount("")
+		if validatePublicKey(acc.PublicKey) {
+			break
+		}
+	}
+	kid := kadIdFromPubkey(acc.PublicKey)
+	return &KadKeyId{
+		PublicKey: acc.PublicKey,
+		Id:        kid,
+	}
+}
+
+func validatePublicKey(pubKey keypair.PublicKey) bool {
+	pub := keypair.SerializePublicKey(pubKey)
+	res := sha256.Sum256(pub)
+	hash := sha256.Sum256(res[:])
+	limit := Difficulty >> 3
+	for i := 0; i < limit; i++ {
+		if hash[i] != 0 {
+			return false
+		}
+	}
+	diff := Difficulty % 8
+	if diff != 0 {
+		x := hash[limit] >> uint8(8-diff)
+		return x == 0
+	}
+	return true
+}
+
+func (self KadId) distance(b KadId) KadId {
+	var c KadId
+	for i := 0; i < len(self.val); i++ {
+		c.val[i] = self.val[i] ^ b.val[i]
+	}
+
+	return c
+}
+
+// Closer returns true if a is closer to self than b is
+func (self KadId) Closer(a, b KadId) bool {
+	adist := self.distance(a)
+	bdist := self.distance(b)
+
+	return bytes.Compare(adist.val[:], bdist.val[:]) < 0
+}
+
+// CommonPrefixLen(cpl) calculate two ID's xor prefix 0
+func CommonPrefixLen(a, b KadId) int {
+	dis := a.distance(b)
+	return zeroPrefixLen(dis.val[:])
+}
+
+// ZeroPrefixLen returns the number of consecutive zeroes in a byte slice.
+func zeroPrefixLen(id []byte) int {
+	for i, b := range id {
+		if b != 0 {
+			return i*8 + bits.LeadingZeros8(uint8(b))
+		}
+	}
+
+	return len(id) * 8
+}

p2pserver/message/msg_pack/msg_pack.go
@@ -22,15 +22,15 @@
 package msgpack
 
 import (
-	"time"
-
 	"github.com/DNAProject/DNA/common"
 	"github.com/DNAProject/DNA/common/config"
 	"github.com/DNAProject/DNA/common/log"
 	ct "github.com/DNAProject/DNA/core/types"
 	msgCommon "github.com/DNAProject/DNA/p2pserver/common"
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
 	mt "github.com/DNAProject/DNA/p2pserver/message/types"
 	p2pnet "github.com/DNAProject/DNA/p2pserver/net/protocol"
+	"time"
 )
 
 //Peer address package
@@ -142,6 +142,14 @@ func NewTxn(txn *ct.Transaction) mt.Message {
 	return &trn
 }
 
+func NewUpdateKadKeyId(n p2pnet.P2P) mt.Message {
+	log.Trace()
+	upk := &mt.UpdateKadId{
+		KadKeyId: n.GetKadKeyId(),
+	}
+	return upk
+}
+
 //version ack package
 func NewVerAck() mt.Message {
 	log.Trace()
@@ -207,4 +215,12 @@ func NewConsensusDataReq(hash common.Uint256) mt.Message {
 	dataReq.Hash = hash
 
 	return &dataReq
+}
+
+func NewFindNodeReq(id kbucket.KadId) mt.Message {
+	req := mt.FindNodeReq{
+		TargetID: id,
+	}
+
+	return &req
 }

p2pserver/message/types/find_node_test.go
@@ -1,0 +1,49 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright 2019 DNA Dev team
+//
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package types
+
+import (
+	"testing"
+
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
+)
+
+func TestFindNodeRequest(t *testing.T) {
+	var req FindNodeReq
+	req.TargetID = kbucket.KadId{}
+
+	MessageTest(t, &req)
+}
+
+func TestFindNodeResponse(t *testing.T) {
+	var resp FindNodeResp
+	resp.TargetID = kbucket.KadId{}
+	resp.Address = "127.0.0.1:1222"
+	resp.CloserPeers = []PeerAddr{
+		PeerAddr{
+			PeerID: 0x456,
+			Addr:   "127.0.0.1:4222",
+		},
+	}
+	resp.Success = true
+
+	MessageTest(t, &resp)
+}

p2pserver/net/protocol/server.go
@@ -24,8 +24,10 @@ package p2p
 
 import (
 	"github.com/DNAProject/DNA/p2pserver/common"
+	"github.com/DNAProject/DNA/p2pserver/dht/kbucket"
 	"github.com/DNAProject/DNA/p2pserver/message/types"
 	"github.com/DNAProject/DNA/p2pserver/peer"
+	"github.com/ontio/ontology-eventbus/actor"
 )
 
 //P2P represent the net interface of p2p package
@@ -34,6 +36,7 @@ type P2P interface {
 	Halt()
 	Connect(addr string) error
 	GetID() uint64
+	GetKId() kbucket.KadId
 	GetVersion() uint32
 	GetPort() uint16
 	GetHttpInfoPort() uint16
@@ -46,7 +49,7 @@ type P2P interface {
 	GetConnectionCnt() uint32
 	GetMaxPeerBlockHeight() uint64
 	GetNp() *peer.NbrPeers
-	GetPeer(uint64) *peer.Peer
+	GetPeer(id uint64) *peer.Peer
 	SetHeight(uint64)
 	IsPeerEstablished(p *peer.Peer) bool
 	Send(p *peer.Peer, msg types.Message) error
@@ -62,9 +65,17 @@ type P2P interface {
 	RemovePeerAddress(addr string)
 	AddNbrNode(*peer.Peer)
 	DelNbrNode(id uint64) (*peer.Peer, bool)
-	NodeEstablished(uint64) bool
+	NodeEstablished(id uint64) bool
 	Xmit(msg types.Message)
 	SetOwnAddress(addr string)
 	IsOwnAddress(addr string) bool
 	IsAddrFromConnecting(addr string) bool
+
+	UpdateDHT(id kbucket.KadId) bool
+	RemoveDHT(id kbucket.KadId) bool
+	BetterPeers(id kbucket.KadId, count int) []kbucket.KadId
+	GetKadKeyId() *kbucket.KadKeyId
+
+	GetPeerStringAddr() map[uint64]string
+	SetPID(pid *actor.PID)
 }